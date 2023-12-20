3c80ce9e4caccbe201ffae49f8c77b3c5f940617
mikekks <thdalsrb79@naver.com>
2023-12-21 01:00:53 +0900

    [ADD]: 프로젝트 세팅

---
+++ src/main/java/synk/meeteam/MeeteamApplication.java
@@ -0,0 +1,13 @@
+package synk.meeteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeteamApplication.class, args);
+	}
+
+}
