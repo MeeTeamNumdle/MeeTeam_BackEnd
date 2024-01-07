9f4733ab3810420fd0d14c0e2309d048b4e28881
mikekks <thdalsrb79@naver.com>
2024-01-07 15:25:54 +0900

    [FEAT] 전역 에러 처리 클래스 추가

---
+++ src/main/java/synk/meeteam/global/common/exception/BaseCustomException.java
@@ -0,0 +1,16 @@
+package synk.meeteam.global.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class BaseCustomException extends RuntimeException {
+
+    private final ExceptionType exceptionType;
+
+    @Override
+    public String getMessage() {
+        return exceptionType.message();
+    }
+}
