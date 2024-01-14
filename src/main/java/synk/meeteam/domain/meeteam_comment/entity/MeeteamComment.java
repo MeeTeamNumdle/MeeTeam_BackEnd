685ede9bff80f3453fd340238bc408cb7e15da77
goder-0 <goder0@naver.com>
2024-01-15 01:36:22 +0900

    [ADD] 널 조건 및 유니크 제약 조건 추가, default value 설정

--- src/main/java/synk/meeteam/domain/meeteam_comment/entity/MeeteamComment.java
+++ src/main/java/synk/meeteam/domain/meeteam_comment/entity/MeeteamComment.java
@@ -9,6 +9,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,19 +24,28 @@ import synk.meeteam.domain.user.entity.User;
 @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "meeteam_comment_uk",
+                columnNames = {"meeteam_id", "writer_id"}
+        )
+})
 public class MeeteamComment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "meeteam_comment_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "meeteam_id")
     private Meeteam meeteam;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "writer_id")
     private User writer;
 
+    @NotNull
+    @Size(max = 100)
+    @Column(length = 100)
     private String content;
 }
