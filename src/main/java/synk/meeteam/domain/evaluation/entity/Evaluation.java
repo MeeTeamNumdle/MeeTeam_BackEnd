731d10f06ac825088afb9ab545944892ab3e2453
goder-0 <goder0@naver.com>
2024-01-15 01:24:57 +0900

    [ADD] 널 조건 및 유니크 제약 조건, 문자열 길이 제한 추가

--- src/main/java/synk/meeteam/domain/evaluation/entity/Evaluation.java
+++ src/main/java/synk/meeteam/domain/evaluation/entity/Evaluation.java
@@ -10,6 +10,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,29 +25,43 @@ import synk.meeteam.domain.user.entity.User;
 @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "evaluation_uk",
+                columnNames = {"user_id", "member_id"}
+        )
+})
 public class Evaluation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "evaluation_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @Size(max = 100)
+    @Column(length = 100)
+    @NotNull
     private String comment;
 
+    @NotNull
     private Long scoreTime;
 
+    @NotNull
     private Long scoreInfluence;
 
+    @NotNull
     private Long scoreParticipation;
 
+    @NotNull
     private Long scoreProfessionalism;
 
+    @NotNull
     private Boolean blind;
 }
