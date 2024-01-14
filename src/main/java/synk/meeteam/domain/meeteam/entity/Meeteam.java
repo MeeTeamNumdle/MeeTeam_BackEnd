ec0b837d178daeeb345c4c2c719db5b4ae901217
goder-0 <goder0@naver.com>
2024-01-15 01:28:32 +0900

    [ADD] 널 조건 및 유니크 제약 조건 추가, default value 설정

--- src/main/java/synk/meeteam/domain/meeteam/entity/Meeteam.java
+++ src/main/java/synk/meeteam/domain/meeteam/entity/Meeteam.java
@@ -11,11 +11,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import synk.meeteam.domain.field.entity.Field;
 import synk.meeteam.domain.user.entity.User;
 
@@ -29,13 +32,19 @@ public class Meeteam {
     @Column(name = "meeteam_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "user_id")
+    @Column(unique = true)
     private User leader;
 
-    private Long likeCount;
-
+    @NotNull
+    @Column(length = 50)
+    @Size(max = 50)
     private String name;
+
+    @NotNull
+    @ColumnDefault("0")
+    private Long likeCount = 0L;
 
     //소개
     @Column(columnDefinition = "TEXT")
@@ -46,18 +55,24 @@ public class Meeteam {
     private MeeteamStatus meeteamStatus;
 
     //구인 글 존재 여부
-    private Boolean isRecruiting;
+    @NotNull
+    @ColumnDefault("0")
+    private Boolean isRecruiting = false;
 
     //기간 시작일
+    @NotNull
     private LocalDateTime proceedingStart;
 
     //기간 종료일
+    @NotNull
     private LocalDateTime proceedingEnd;
 
     //공개 여부
-    private Boolean isPublic;
+    @NotNull
+    @ColumnDefault("1")
+    private Boolean isPublic = true;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "field_id")
     private Field field;
 }
