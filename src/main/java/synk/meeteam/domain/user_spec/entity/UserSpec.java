35c8d46ff9e3359cdf001e70f219f6e4512cc47f
goder-0 <goder0@naver.com>
2024-01-15 18:08:11 +0900

    [ADD] 유니크 키 생성, 널 조건 추가

--- src/main/java/synk/meeteam/domain/user_spec/entity/UserSpec.java
+++ src/main/java/synk/meeteam/domain/user_spec/entity/UserSpec.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,17 +22,23 @@ import synk.meeteam.domain.user.entity.User;
 @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "user_spec_uk",
+                columnNames = {"user_id", "spec_id"}
+        )
+})
 public class UserSpec {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_spec_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "spec_id")
     private Spec spec;
 }
