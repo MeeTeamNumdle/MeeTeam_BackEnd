e5644c4c6f9b8bb5f97576e481621162b01dcb34
goder-0 <goder0@naver.com>
2024-01-15 18:00:41 +0900

    [ADD] 널 조건, 길이 조건 추가

--- src/main/java/synk/meeteam/domain/timetable_block/entity/TimetableBlock.java
+++ src/main/java/synk/meeteam/domain/timetable_block/entity/TimetableBlock.java
@@ -4,11 +4,15 @@ import static jakarta.persistence.FetchType.LAZY;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.DayOfWeek;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -26,13 +30,22 @@ public class TimetableBlock {
     @Column(name = "timetable_block_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "user_id")
     private User user;
 
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
     private DayOfWeek dayOfWeek;
 
+    @NotNull
+    @Size(max = 5)
+    @Column(length = 5)
     private String startTime;
 
+    @NotNull
+    @Size(max = 5)
+    @Column(length = 5)
     private String endTime;
 }
