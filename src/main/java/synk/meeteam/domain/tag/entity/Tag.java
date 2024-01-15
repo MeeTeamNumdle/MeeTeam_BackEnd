2b5f062958b3104774dced84456014c7b822b077
goder-0 <goder0@naver.com>
2024-01-15 18:00:01 +0900

    [ADD] 타입 필드 추가, 널 조건 및 길이 조건 추가

--- src/main/java/synk/meeteam/domain/tag/entity/Tag.java
+++ src/main/java/synk/meeteam/domain/tag/entity/Tag.java
@@ -2,13 +2,17 @@ package synk.meeteam.domain.tag.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @Setter
@@ -20,5 +24,13 @@ public class Tag {
     @Column(name = "tag_id")
     private Long id;
 
+    @NotNull
+    @Column(length = 50)
     private String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("MEETEAM")
+    @Column(length = 10)
+    private TagType type = TagType.MEETEAM;
 }
