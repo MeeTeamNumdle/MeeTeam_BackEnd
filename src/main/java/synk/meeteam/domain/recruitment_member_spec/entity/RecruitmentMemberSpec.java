aa27fb7c5d63e2427c728f7eb13ff13f098ad456
goder-0 <goder0@naver.com>
2024-01-15 17:20:19 +0900

    [ADD] 널 조건 추가, 유니크 키 생성

--- src/main/java/synk/meeteam/domain/recruitment_member_spec/entity/RecruitmentMemberSpec.java
+++ src/main/java/synk/meeteam/domain/recruitment_member_spec/entity/RecruitmentMemberSpec.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,17 +22,23 @@ import synk.meeteam.domain.recruitment_member.entity.RecruitmentMember;
 @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "recruitment_member_uk",
+                columnNames = {"recruitment_member_id", "spec_id"}
+        )
+})
 public class RecruitmentMemberSpec {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "recruitment_member_spec_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "recruitment_member_id")
     private RecruitmentMember recruitmentMember;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "spec_id")
     private Spec spec;
 }
