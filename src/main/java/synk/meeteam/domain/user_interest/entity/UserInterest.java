283590a5d23088756ae7183eca51a824ce5ed30d
goder-0 <goder0@naver.com>
2024-01-15 18:06:30 +0900

    [ADD] 유니크 키 생성, 널 조건 추가

--- src/main/java/synk/meeteam/domain/user_interest/entity/UserInterest.java
+++ src/main/java/synk/meeteam/domain/user_interest/entity/UserInterest.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,17 +22,23 @@ import synk.meeteam.domain.user.entity.User;
 @Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(
+                name = "user_interest_uk",
+                columnNames = {"user_id", "interest_role_id"}
+        )
+})
 public class UserInterest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_interest_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "interest_id")
+    @ManyToOne(fetch = LAZY, optional = false)
+    @JoinColumn(name = "interest_role_id")
     private Role role;
 }
