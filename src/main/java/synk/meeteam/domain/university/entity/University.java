a67c4e13c61856475ffde1ef2ba93a1cacfcf0b5
goder-0 <goder0@naver.com>
2024-01-15 18:00:54 +0900

    [ADD] 널 조건, 길이 조건 추가

--- src/main/java/synk/meeteam/domain/university/entity/University.java
+++ src/main/java/synk/meeteam/domain/university/entity/University.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,9 +21,15 @@ public class University {
     @Column(name = "university_id")
     private Long id;
 
+    @NotNull
+    @Column(length = 20)
     private String schoolName;
 
+    @NotNull
+    @Column(length = 20)
     private String departmentName;
 
+    @NotNull
+    @Column(length = 20)
     private String email_regex;
 }
