a6b15f7a92a94e9b22da9b1f01234f028e778be3
goder-0 <goder0@naver.com>
2024-01-15 18:01:46 +0900

    [ADD] 소개 관련 필드 및 타입 필드 추가, 널 조건, 길이 조건 추가

--- src/main/java/synk/meeteam/domain/user/entity/User.java
+++ src/main/java/synk/meeteam/domain/user/entity/User.java
@@ -4,11 +4,15 @@ import static jakarta.persistence.FetchType.LAZY;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,27 +29,54 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @NotNull
+    @Size(max = 100)
+    @Column(length = 100)
     private String email;
 
+    @NotNull
+    @Size(max = 20)
+    @Column(length = 20)
     private String name;
 
+    @NotNull
+    @Size(max = 20)
+    @Column(length = 20, unique = true)
     private String nickname;
 
+    @Column(length = 100)
     private String password;
 
+    @Column(length = 11)
     private String phoneNumber;
+
+    //한줄 소개
+    @Column(length = 20)
+    private String oneLineIntroduction;
+
+    //자기 소개
+    @Column(columnDefinition = "TEXT")
+    private String mainIntroduction;
 
     private Double gpa;
 
+    @NotNull
     private Integer admissionYear;
 
+    @Column(length = 300)
     private String pictureUrl;
 
     //평가 점수
     private String evaluationScore;
 
+    //계정 타입
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(length = 15)
+    private UserType type;
+
     //학사 정보
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = LAZY, optional = false)
     @JoinColumn(name = "university_id")
     private University university;
 }
