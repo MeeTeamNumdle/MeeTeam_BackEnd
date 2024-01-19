d2036a61803ed7c63d2fd61f2b0cfb8fc8479419
mikekks <thdalsrb79@naver.com>
2024-01-20 02:07:24 +0900

    [FIX] 대학 예외 클래스 변경 (코드리뷰)

--- src/main/java/synk/meeteam/domain/university/repository/UniversityRepository.java
+++ src/main/java/synk/meeteam/domain/university/repository/UniversityRepository.java
@@ -1,19 +1,20 @@
 package synk.meeteam.domain.university.repository;
 
-import static synk.meeteam.domain.auth.exception.AuthExceptionType.NOT_FOUND_EMAIL_REGEX;
 import static synk.meeteam.domain.auth.exception.AuthExceptionType.NOT_FOUND_UNIVERSITY_AND_DEPARTMENT;
+import static synk.meeteam.domain.university.exception.UniversityExceptionType.NOT_FOUND_EMAIL_REGEX;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import synk.meeteam.domain.auth.exception.AuthException;
 import synk.meeteam.domain.university.entity.University;
+import synk.meeteam.domain.university.exception.UniversityException;
 
 public interface UniversityRepository extends JpaRepository<University, Long> {
 
     Optional<University> findByUniversityName(String universityName);
 
     default University findByUniversityNameOrElseThrowException(String universityName) {
-        return findByUniversityName(universityName).orElseThrow(() -> new AuthException(NOT_FOUND_EMAIL_REGEX));
+        return findByUniversityName(universityName).orElseThrow(() -> new UniversityException(NOT_FOUND_EMAIL_REGEX));
     }
 
     Optional<University> findByUniversityNameAndDepartmentName(String universityName, String departmentName);
