b3fdccb604d5ca339ffa0bce31cb2df42f52eaa3
mikekks <thdalsrb79@naver.com>
2024-01-20 02:07:05 +0900

    [FEAT] 대학 예외 enum 추가 (코드리뷰)

--- src/main/java/synk/meeteam/domain/university/exception/UniversityExceptionType.java
+++ src/main/java/synk/meeteam/domain/university/exception/UniversityExceptionType.java
@@ -1,18 +1,25 @@
 package synk.meeteam.domain.university.exception;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import synk.meeteam.global.common.exception.ExceptionType;
 
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum UniversityExceptionType implements ExceptionType {
-    ;
+
+    NOT_FOUND_EMAIL_REGEX(HttpStatus.NOT_FOUND, "유효한 학교 이메일이 아닙니다.");
+
+    private final HttpStatus status;
+    private final String message;
 
     @Override
     public HttpStatus httpStatus() {
-        return null;
+        return this.status;
     }
 
     @Override
     public String message() {
-        return null;
+        return this.message;
     }
 }
