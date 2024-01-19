d6524eca6105510acf1635648523c520d04276ee
mikekks <thdalsrb79@naver.com>
2024-01-20 02:06:23 +0900

    [DEL] 필요없는 코드 삭제 (코드리뷰)

--- src/main/java/synk/meeteam/security/config/SecurityConfig.java
+++ src/main/java/synk/meeteam/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -72,9 +73,7 @@ public class SecurityConfig {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .headers((headerConfig) ->
-                        headerConfig.frameOptions(frameOptionsConfig ->
-                                frameOptionsConfig.disable()
-                        )
+                        headerConfig.frameOptions(FrameOptionsConfig::disable)
                 )
               //  .userDetailsService(memberAuthService)
         ;
