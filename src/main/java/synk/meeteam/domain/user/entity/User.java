349089a4f687445bf220ef08042faa732d04bd84
mikekks <thdalsrb79@naver.com>
2024-01-20 01:48:47 +0900

    [CHORE] Builder 추가

--- src/main/java/synk/meeteam/domain/user/entity/User.java
+++ src/main/java/synk/meeteam/domain/user/entity/User.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -120,5 +121,20 @@ public class User extends BaseTimeEntity {
     // 비밀번호 암호화 메소드
     public void passwordEncode(PasswordEncoder passwordEncoder) {
         this.password = passwordEncoder.encode(this.password);
+    }
+
+    @Builder
+    public User(String email, String name, String nickname, String password, String phoneNumber,
+                Integer admissionYear, UserType type, Role role, PlatformType platformType, String platformId) {
+        this.email = email;
+        this.name = name;
+        this.nickname = nickname;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.admissionYear = admissionYear;
+        this.type = type;
+        this.role = role;
+        this.platformType = platformType;
+        this.platformId = platformId;
     }
 }
