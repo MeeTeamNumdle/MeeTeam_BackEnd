94f2bb598fa011933f6ed1df884cce9749b7a98c
mikekks <thdalsrb79@naver.com>
2024-01-31 01:11:25 +0900

    [CHORE] 필요없는 코드 삭제 및 getUserName 메서드 수정

--- src/main/java/synk/meeteam/domain/user/service/UserService.java
+++ src/main/java/synk/meeteam/domain/user/service/UserService.java
@@ -2,9 +2,16 @@ package synk.meeteam.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import synk.meeteam.domain.user.entity.User;
+import synk.meeteam.domain.user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
 
+    public String getUserName(User user){
+        return userRepository.findByPlatformIdOrElseThrowException(
+                user.getPlatformId()).getName();
+    }
 }
