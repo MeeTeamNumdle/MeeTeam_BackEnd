f15e9f7397eb6b77d86889f1e6faab1aed4c93a1
mikekks <thdalsrb79@naver.com>
2024-01-30 18:17:12 +0900

    [ADD] 구인 멤버 dao 추가

---
+++ src/main/java/synk/meeteam/domain/recruitment_member/repository/RecruitmentMemberRepository.java
@@ -0,0 +1,8 @@
+package synk.meeteam.domain.recruitment_member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import synk.meeteam.domain.recruitment_member.entity.RecruitmentMember;
+
+public interface RecruitmentMemberRepository extends JpaRepository<RecruitmentMember, Long> {
+
+}
