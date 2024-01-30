4dcfbf7bca27ddfff1c0a500776c61c31a8fd8b6
mikekks <thdalsrb79@naver.com>
2024-01-30 18:11:09 +0900

    [FEAT] request validation 핸들링 추가

--- src/main/java/synk/meeteam/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/synk/meeteam/global/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package synk.meeteam.global.common.exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,6 +23,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(exceptionType.httpStatus())
                 .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
     }
+
+    // 반환 형태 수정 필요
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> dtoValidation(final MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error)-> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
+
 
 }
 
