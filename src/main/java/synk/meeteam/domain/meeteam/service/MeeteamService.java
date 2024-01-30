f5f2c3a861934b801fe8cdfa3a308e8099af5258
mikekks <thdalsrb79@naver.com>
2024-01-30 18:13:35 +0900

    [FEAT] 밋팀 생성 메서드 구현

---
+++ src/main/java/synk/meeteam/domain/meeteam/service/MeeteamService.java
@@ -0,0 +1,57 @@
+package synk.meeteam.domain.meeteam.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import synk.meeteam.domain.meeteam.dto.request.CreateMeeteamRequestDto;
+import synk.meeteam.domain.meeteam.entity.Meeteam;
+import synk.meeteam.domain.meeteam.repository.MeeteamRepository;
+import synk.meeteam.domain.meeteam_tag.entity.MeeteamTag;
+import synk.meeteam.domain.meeteam_tag.repository.MeeteamTagRepository;
+import synk.meeteam.domain.tag.entity.Tag;
+import synk.meeteam.domain.user.entity.User;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MeeteamService {
+    private final MeeteamRepository meeteamRepository;
+    private final MeeteamTagRepository meeteamTagRepository;
+
+    @Transactional
+    public Meeteam createMeeteam(final CreateMeeteamRequestDto requestDto, final User user) {
+        Meeteam newMeeteam = Meeteam.builder()
+                .leader(user)
+                .name(requestDto.name())
+                .introduction(requestDto.introduction())
+                .isRecruiting(requestDto.isRecruiting())
+                .isCourse(requestDto.isCourse())
+                .meeteamScope(requestDto.meeteamScope())
+                .meeteamCategory(requestDto.meeteamCategory())
+                .meeteamProceed(requestDto.meeteamProceed())
+                .proceedingStart(requestDto.proceedingStart())
+                .proceedingEnd(requestDto.proceedingEnd())
+                .isPublic(requestDto.isPublic())
+                .field(requestDto.field())
+                .coverImageUrl(requestDto.coverImgUrl())
+                .build();
+
+        meeteamRepository.saveAndFlush(newMeeteam);
+
+        createMeeteamTags(requestDto.meeteamTags(), newMeeteam);
+
+        return newMeeteam;
+    }
+
+
+    private void createMeeteamTags(List<Tag> meeteamTags, Meeteam meeteam){
+        for(Tag curTag : meeteamTags){
+            MeeteamTag meeteamTag = MeeteamTag.builder()
+                    .meeteam(meeteam)
+                    .tag(curTag)
+                    .build();
+            meeteamTagRepository.save(meeteamTag);
+        }
+    }
+}
