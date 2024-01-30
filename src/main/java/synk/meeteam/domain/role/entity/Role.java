cddcf08f7a98b4e35e3af5bbdf89bc3428bf1fb8
mikekks <thdalsrb79@naver.com>
2024-01-30 18:18:39 +0900

    [ADD] 빌더 추가

--- src/main/java/synk/meeteam/domain/role/entity/Role.java
+++ src/main/java/synk/meeteam/domain/role/entity/Role.java
@@ -7,12 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Role {
@@ -24,4 +23,9 @@ public class Role {
     @NotNull
     @Column(length = 50)
     private String name;
+
+    @Builder
+    public Role(String name) {
+        this.name = name;
+    }
 }
