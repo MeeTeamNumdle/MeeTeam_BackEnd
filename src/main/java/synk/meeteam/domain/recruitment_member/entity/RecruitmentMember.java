9146b496d93482c5798c67005f194d64a2b2c627
mikekks <thdalsrb79@naver.com>
2024-01-30 18:16:57 +0900

    [ADD] 빌더 추가

--- src/main/java/synk/meeteam/domain/recruitment_member/entity/RecruitmentMember.java
+++ src/main/java/synk/meeteam/domain/recruitment_member/entity/RecruitmentMember.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -36,5 +37,12 @@ public class RecruitmentMember {
     private Role role;
 
     @NotNull
-    private Long count;
+    private int count;
+
+    @Builder
+    public RecruitmentMember(Recruitment recruitment, Role role, int count) {
+        this.recruitment = recruitment;
+        this.role = role;
+        this.count = count;
+    }
 }
