6af50b301eee3e0386a9f14e7162a974388f78a8
mikekks <thdalsrb79@naver.com>
2024-01-30 18:13:19 +0900

    [ADD] 밋팀 dao 추가

---
+++ src/main/java/synk/meeteam/domain/meeteam/repository/MeeteamRepository.java
@@ -0,0 +1,8 @@
+package synk.meeteam.domain.meeteam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import synk.meeteam.domain.meeteam.entity.Meeteam;
+
+public interface MeeteamRepository extends JpaRepository<Meeteam, Long> {
+
+}
