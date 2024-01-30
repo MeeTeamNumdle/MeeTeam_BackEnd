d2dd3655c59487ced389d8a04e24aba8f7b3ab6f
mikekks <thdalsrb79@naver.com>
2024-01-30 18:14:02 +0900

    [ADD] 밋팀 태그 dao 추가

---
+++ src/main/java/synk/meeteam/domain/meeteam_tag/repository/MeeteamTagRepository.java
@@ -0,0 +1,8 @@
+package synk.meeteam.domain.meeteam_tag.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import synk.meeteam.domain.meeteam_tag.entity.MeeteamTag;
+
+public interface MeeteamTagRepository extends JpaRepository<MeeteamTag, Long> {
+
+}
