7e2b6aa4e59a0a762b2e49cc5ca30846fc8e3bd8
mikekks <thdalsrb79@naver.com>
2024-01-30 18:17:55 +0900

    [ADD] 구인멤버 스펙 dao 추가

---
+++ src/main/java/synk/meeteam/domain/recruitment_member_spec/repository/RecruitmentMemberSpecRepository.java
@@ -0,0 +1,8 @@
+package synk.meeteam.domain.recruitment_member_spec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import synk.meeteam.domain.recruitment_member_spec.entity.RecruitmentMemberSpec;
+
+public interface RecruitmentMemberSpecRepository extends JpaRepository<RecruitmentMemberSpec, Long> {
+
+}
