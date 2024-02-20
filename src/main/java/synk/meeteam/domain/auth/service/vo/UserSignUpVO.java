9ac0bd1f2ea66bd8fdebe3e3aee3493de7696ec0
mikekks <thdalsrb79@naver.com>
2024-02-20 21:25:45 +0900

    [CHORE] 프로필 이미지 추가

--- src/main/java/synk/meeteam/domain/auth/service/vo/UserSignUpVO.java
+++ src/main/java/synk/meeteam/domain/auth/service/vo/UserSignUpVO.java
@@ -8,12 +8,14 @@ import synk.meeteam.domain.user.user.entity.enums.PlatformType;
 import synk.meeteam.infra.oauth.service.vo.enums.AuthType;
 
 @Builder
-public record UserSignUpVO(Long userId, String email, String name, PlatformType platformType, Authority authority,
-                           String platformId, String phoneNumber, AuthType authType) {
+public record UserSignUpVO(Long userId, String email, String name, String pictureUrl, PlatformType platformType,
+                           Authority authority, String platformId, String phoneNumber, AuthType authType) {
     public static UserSignUpVO of(User user, PlatformType platformType, Authority authority, AuthType authType) {
         return UserSignUpVO.builder()
+                .userId(user.getId())
                 .email(user.getEmail())
                 .name(user.getName())
+                .pictureUrl(user.getPictureUrl())
                 .platformType(platformType)
                 .platformId(user.getPlatformId())
                 .authority(authority)
