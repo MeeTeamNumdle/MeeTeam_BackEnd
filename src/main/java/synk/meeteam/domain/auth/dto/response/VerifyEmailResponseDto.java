d0ea195d7b46e5299be98515f031a75fec50f3bb
mikekks <thdalsrb79@naver.com>
2024-02-20 21:26:48 +0900

    [RENAME] Dto 명 변경

--- src/main/java/synk/meeteam/domain/auth/dto/response/VerifyEmailResponseDto.java
+++ src/main/java/synk/meeteam/domain/auth/dto/response/VerifyEmailResponseDto.java
@@ -4,12 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(name = "SignUpUserResponseDto", description = "회원가입 응답 Dto")
-public record SignUpUserResponseDto(
+public record VerifyEmailResponseDto(
         @NotNull
         @Schema(description = "플랫폼 Id", example = "Di7lChMGxjZVTai6d76Ho1YLDU_xL8tl1CfdPMV5SQM")
         String  platformId
         ) {
-    public static SignUpUserResponseDto of(String platformId) {
-        return new SignUpUserResponseDto(platformId);
+    public static VerifyEmailResponseDto of(String platformId) {
+        return new VerifyEmailResponseDto(platformId);
     }
 }
