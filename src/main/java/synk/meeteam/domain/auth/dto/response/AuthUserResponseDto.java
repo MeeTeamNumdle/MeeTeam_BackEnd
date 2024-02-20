503ac79db09a91451a820ff38612b93f0e2dc3fd
mikekks <thdalsrb79@naver.com>
2024-02-20 21:18:42 +0900

    [CHORE] 1. userId와 platformId 모두 존재하게끔 변경 2. 프로필 사진 추가

--- src/main/java/synk/meeteam/domain/auth/dto/response/AuthUserResponseDto.java
+++ src/main/java/synk/meeteam/domain/auth/dto/response/AuthUserResponseDto.java
@@ -7,24 +7,28 @@ import synk.meeteam.infra.oauth.service.vo.enums.AuthType;
 
 @Schema(name = "AuthUserResponseDto", description = "소셜 로그인 응답 Dto")
 public record AuthUserResponseDto(
-        @NotNull
-        @Schema(description = "플랫폼 Id", example = "Di7lChMGxjZVTai6d76Ho1YLDU_xL8tl1CfdPMV5SQM")
+        @Schema(description = "user Id", example = "4OaVE421DSwR63xfKf6vxA==(200) or null(201)")
+        String userId,
+        @Schema(description = "플랫폼 Id", example = "null(200) or Di7lChMGxjZVTai6d76Ho1YLDU_xL8tl1CfdPMV5SQM(201)")
         String platformId,
         @NotNull
-        @Schema(description = "인가 타입(LOGIN or SIGN_UP)", example = "LOGIN")
+        @Schema(description = "인가 타입(LOGIN or SIGN_UP)", example = "LOGIN(200) or SIGN_UP(201)")
         AuthType authType,
         @NotNull
         @Schema(description = "유저 이름", example = "송민규")
         String userName,
         @NotNull
-        @Schema(description = "역할, GUEST는 임시 유저", example = "USER")
+        @Schema(description = "유저 프로필 사진", example = "url 형태")
+        String pictureUrl,
+        @NotNull
+        @Schema(description = "역할, GUEST는 임시 유저", example = "USER(200) or GUEST(201)")
         Authority authority,
-        @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsInBsYXRmb3JtSWQiOiJEaTdsQ2hNR3hqWlZUYWk2ZDc2SG8xWUxEVV94TDh0bDFDZmRQTVY1U1FNIiwicGxhdGZvcm1UeXBlIjoiTkFWRVIiLCJpYXQiOjE3MDYyODA1MjMsImV4cCI6MTgxNDI4MDUyM30.doPtAdLQMZ8NeuhRAOg7GNMBBtFZzPOOZp60HskGtZ0")
+        @Schema(description = "액세스 토큰", example = "null(201) or 액세스토큰 eyJhbGciOiJIUzI1N~~ (200)")
         String accessToken,
-        @Schema(description = "리프레시 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJSZWZyZXNoVG9rZW4iLCJpYXQiOjE3MDYyODA1MjMsImV4cCI6MTcwNjg4NTMyM30.yvftTGVld0ZMnv1a79wpuzuTo8EJ1zOHoSlT_jfH3cs")
+        @Schema(description = "리프레시 토큰", example = "null(201) or 리프레시 토큰 eyJhbGciOiJIUzI1N~~ (200)")
         String refreshToken) {
-    public static AuthUserResponseDto of(String platformId, AuthType authType, String userName, Authority authority,
+    public static AuthUserResponseDto of(String userId, String platformId, AuthType authType, String userName, String pictureUrl, Authority authority,
                                          String accessToken, String refreshToken) {
-        return new AuthUserResponseDto(platformId, authType, userName, authority, accessToken, refreshToken);
+        return new AuthUserResponseDto(userId, platformId, authType, userName, pictureUrl, authority, accessToken, refreshToken);
     }
 }
