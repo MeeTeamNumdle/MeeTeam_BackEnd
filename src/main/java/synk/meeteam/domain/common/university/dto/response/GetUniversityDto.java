a064330257c6893aa346ada09ed3b03d174d9761
mikekks <thdalsrb79@naver.com>
2024-02-24 13:31:03 +0900

    [ADD] 대학교 Dto 추가

---
+++ src/main/java/synk/meeteam/domain/common/university/dto/response/GetUniversityDto.java
@@ -0,0 +1,23 @@
+package synk.meeteam.domain.common.university.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import synk.meeteam.domain.common.university.entity.University;
+
+@Builder
+public record GetUniversityDto(
+        @Schema(description = "대학교 id", example = "1")
+        Long universityId,
+        @Schema(description = "대학교 이름", example = "광운대학교")
+        String universityName,
+        @Schema(description = "대학교 이메일 도메인", example = "kw.ac.kr")
+        String universityDomain
+) {
+    public static GetUniversityDto of(University university) {
+        return GetUniversityDto.builder()
+                .universityId(university.getId())
+                .universityName(university.getName())
+                .universityDomain(university.getEmailRegex())
+                .build();
+    }
+}
