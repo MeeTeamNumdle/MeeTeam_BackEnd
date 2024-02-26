e83cfd20d6c5f0b8766fa86305383146228e4ad4
mikekks <thdalsrb79@naver.com>
2024-02-26 23:34:17 +0900

    [ADD] 트랜잭션 추가

--- src/main/java/synk/meeteam/domain/common/department/service/DepartmentService.java
+++ src/main/java/synk/meeteam/domain/common/department/service/DepartmentService.java
@@ -3,6 +3,7 @@ package synk.meeteam.domain.common.department.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import synk.meeteam.domain.common.department.entity.Department;
 import synk.meeteam.domain.common.department.repository.DepartmentRepository;
 import synk.meeteam.domain.common.university.entity.University;
@@ -13,6 +14,7 @@ public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
 
+    @Transactional(readOnly = true)
     public List<Department> getDepartmentsByUniversity(University university) {
         List<Department> departments = departmentRepository.findAllByUniversity(university);
         return departments;
