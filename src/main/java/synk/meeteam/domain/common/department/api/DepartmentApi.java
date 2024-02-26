6e75f1d8d520404d1c1023ab8fdbb8066340a1cc
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-02-26 20:38:28 +0900

    [DOCS] 특정 학교의 학과 리스트 조회 API (#52)
    * [DOCS] 특정 학교의 학과 리스트 조회 API
    
    * [DOCS] 특정 학교의 학과 리스트 조회 API 일부수정
    
    * [DOCS] 쿼리 param 일부 수정

---
+++ src/main/java/synk/meeteam/domain/common/department/api/DepartmentApi.java
@@ -0,0 +1,24 @@
+package synk.meeteam.domain.common.department.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import synk.meeteam.domain.common.department.dto.GetDepartmentResponseDto;
+
+@Tag(name = "department", description = "학과 관련 API")
+public interface DepartmentApi {
+
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "학과 조회 성공"),
+            }
+    )
+    @Operation(summary = "특정 학교의 학과 조회 API")
+    @SecurityRequirements
+    ResponseEntity<List<GetDepartmentResponseDto>> getDepartments(@RequestParam("university") Long universityId);
+}
