65da6adb28023e5b3dc0ad0d21f96c41280a17a5
mikekks <thdalsrb79@naver.com>
2024-02-29 23:21:54 +0900

    [FEAT] request Dto에 검증 어노테이션 추가

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/request/CreateRecruitmentPostRequestDto.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/request/CreateRecruitmentPostRequestDto.java
@@ -1,48 +1,48 @@
 package synk.meeteam.domain.recruitment.recruitment_post.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
-import synk.meeteam.global.entity.Category;
-import synk.meeteam.global.entity.ProceedType;
-import synk.meeteam.global.entity.Scope;
 
 public record CreateRecruitmentPostRequestDto(
         @NotNull
+        @Column(length = 10)
         @Schema(description = "범위", example = "교내")
-        Scope scope,
+        String scope,
         @NotNull
         @Schema(description = "유형", example = "프로젝트")
-        Category category,
+        String category,
         @NotNull
         @Schema(description = "구인 마감일", example = "2023-02-05")
         LocalDate deadline,
         @NotNull
-        @Schema(description = "시작 기간, 종료기간", example = "[\"2023-02-01\",\"2023-07-01\"]")
+        @Schema(description = "[시작 기간, 종료기간]", example = "[\"2023-02-01\",\"2023-07-01\"]")
         List<LocalDate> proceedingPeriod,
         @NotBlank
         @Schema(description = "분야", example = "개발")
-        String field,
+        Long fieldId,
         @NotNull
         @Schema(description = "진행방식", example = "온라인")
-        ProceedType proceedType,
+        String proceedType,
         @NotNull
         @Schema(description = "수업 관련 정보")
         CourseTagDto courseTag,
         @NotNull
-        @Schema(description = "태그", example = "[웹개발, AI, 졸업작품]")
+        @Schema(description = "태그", example = "[\"웹개발\", \"AI\", \"졸업작품\"]")
         List<String> Tags,
         @NotNull
         @Schema(description = "필요한 역할들(List 형태로)", example = "")
         List<RecruitmentRoleDto> recruitmentRoles,
         @NotBlank
+        @Size(max = 40)
         @Schema(description = "제목", example = "졸작 사람 구합니다!!")
         String title,
         @NotNull
         @Schema(description = "구인 내용", example = "안녕하세요. 사람구하는데 진짜 고수만 구해요..")
         String content
-
 ) {
 }
