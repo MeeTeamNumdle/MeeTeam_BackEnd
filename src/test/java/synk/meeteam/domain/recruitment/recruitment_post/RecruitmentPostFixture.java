fdca426d596af69e7248cc8aef3e930e95bd3088
mikekks <thdalsrb79@naver.com>
2024-02-29 23:17:11 +0900

    [ADD] 테스트 객체 Fixture 추가

---
+++ src/test/java/synk/meeteam/domain/recruitment/recruitment_post/RecruitmentPostFixture.java
@@ -0,0 +1,29 @@
+package synk.meeteam.domain.recruitment.recruitment_post;
+
+import java.time.LocalDate;
+import synk.meeteam.domain.common.field.entity.Field;
+import synk.meeteam.domain.recruitment.recruitment_post.entity.RecruitmentPost;
+import synk.meeteam.global.entity.Category;
+import synk.meeteam.global.entity.ProceedType;
+import synk.meeteam.global.entity.Scope;
+
+public class RecruitmentPostFixture {
+
+    public static String TITLE = "정상제목";
+    public static String TITLE_EXCEED_40 = "구인글 생성을 할 때 이 제목은 예외가 발생해야 하는 제목입니다. 왜냐하면 40자가 넘기 떄문입니다. 왜냐하면 40자가 넘기 떄문입니다. 왜냐하면 40자가 넘기 떄문입니다.";
+    public static Field field = new Field(1L, "개발");
+
+    public static RecruitmentPost createRecruitmentPost(String title) {
+        return RecruitmentPost.builder()
+                .title(title)
+                .content("정상적인 내용")
+                .scope(Scope.ON_CAMPUS)
+                .category(Category.PROJECT)
+                .field(field)
+                .proceedType(ProceedType.ON_LINE)
+                .proceedingStart(LocalDate.of(2024, 2, 28))
+                .proceedingEnd(LocalDate.of(2024, 7, 28))
+                .deadline(LocalDate.of(2024, 3, 2))
+                .build();
+    }
+}
