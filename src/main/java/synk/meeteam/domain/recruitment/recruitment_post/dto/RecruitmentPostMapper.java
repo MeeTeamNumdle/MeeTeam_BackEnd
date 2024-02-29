5db19066ce659ecdb31f8f58acfebb66b1daa7a9
mikekks <thdalsrb79@naver.com>
2024-02-29 23:17:30 +0900

    [FEAT] Dto -> Entity 변환해주는 mapper 구현

---
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/RecruitmentPostMapper.java
@@ -0,0 +1,33 @@
+package synk.meeteam.domain.recruitment.recruitment_post.dto;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import synk.meeteam.domain.recruitment.recruitment_post.dto.request.CreateRecruitmentPostRequestDto;
+import synk.meeteam.domain.recruitment.recruitment_post.entity.RecruitmentPost;
+import synk.meeteam.global.entity.Category;
+import synk.meeteam.global.entity.ProceedType;
+import synk.meeteam.global.entity.Scope;
+
+@Mapper(componentModel = "spring")
+public interface RecruitmentPostMapper {
+    @Named("scopeToEnum")
+    static Scope scopeToEnum(String scope) {
+        return Scope.findByName(scope);
+    }
+
+    @Named("categoryToEnum")
+    static Category categoryToEnum(String category) {
+        return Category.findByName(category);
+    }
+
+    @Named("proceedTypeToEnum")
+    static ProceedType proceedTypeToEnum(String proceedType) {
+        return ProceedType.findByName(proceedType);
+    }
+
+    @Mapping(source = "requestDto.scope", target = "scope", qualifiedByName = "scopeToEnum")
+    @Mapping(source = "requestDto.category", target = "category", qualifiedByName = "categoryToEnum")
+    @Mapping(source = "requestDto.proceedType", target = "proceedType", qualifiedByName = "proceedTypeToEnum")
+    RecruitmentPost toRecruitmentEntity(CreateRecruitmentPostRequestDto requestDto);
+}
