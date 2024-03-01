d279e923a78a46668326967fb7e8016a82d20e21
mikekks <thdalsrb79@naver.com>
2024-03-02 01:00:43 +0900

    [FEAT] 클라이언트 오류들 예외 핸들링

--- src/main/java/synk/meeteam/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/synk/meeteam/global/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,14 @@
 package synk.meeteam.global.common.exception;
 
+import static synk.meeteam.global.common.exception.GlobalExceptionType.INVALID_INPUT_VALUE;
+import static synk.meeteam.global.common.exception.GlobalExceptionType.SERVER_ERROR;
+
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,11 +16,50 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final String LOG_FORMAT = "%s";
-    private static final String SERVER_ERROR_MESSAGE = "서버 오류가 발생하였습니다.";
 
     @ExceptionHandler(BaseCustomException.class)
     public ResponseEntity<ExceptionResponse> handleBaseException(BaseCustomException e) {
         ExceptionType exceptionType = e.getExceptionType();
+        log.info(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)  // @Notnull 오류
+    public ResponseEntity<ExceptionResponse> handleConstraintViolationException(ConstraintViolationException e) {
+        ExceptionType exceptionType = INVALID_INPUT_VALUE;
+        log.info(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)  // null value 오류
+    public ResponseEntity<ExceptionResponse> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        ExceptionType exceptionType = INVALID_INPUT_VALUE;
+        log.info(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)  // null value 오류
+    public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        ExceptionType exceptionType = INVALID_INPUT_VALUE;
+        log.info(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ExceptionType exceptionType = INVALID_INPUT_VALUE;
+        log.info(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        ExceptionType exceptionType = SERVER_ERROR;
         log.info(String.format(LOG_FORMAT, e.getMessage()), e);
         return ResponseEntity.status(exceptionType.httpStatus())
                 .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
