4149ed4d472e8fdd62f270ea0762df7b4167f9e1
mikekks <thdalsrb79@naver.com>
2024-03-01 16:26:11 +0900

    [FEAT] RecruitmentRoleSkillRepository 구현 및 테스트

---
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_role_skill/repository/RecruitmentRoleSkillRepository.java
@@ -0,0 +1,8 @@
+package synk.meeteam.domain.recruitment.recruitment_role_skill.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import synk.meeteam.domain.recruitment.recruitment_role_skill.entity.RecruitmentRoleSkill;
+
+public interface RecruitmentRoleSkillRepository extends JpaRepository<RecruitmentRoleSkill, Long> {
+
+}
