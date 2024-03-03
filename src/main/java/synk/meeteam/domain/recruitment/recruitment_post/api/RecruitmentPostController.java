61b76ff6c42b3814d70c6a7020b53544bc09cb6e
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-03-04 01:20:54 +0900

    [DOCS] 특정 구인글 상세 페이지 조회 API 스웨거 (#67)
    * [DOCS] 특정 구인글 상세 페이지 조회 API 스웨거
    
    * [DOCS] 일부 수정
    
    * [DOCS] 유형 추가

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/api/RecruitmentPostController.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/api/RecruitmentPostController.java
@@ -3,11 +3,16 @@ package synk.meeteam.domain.recruitment.recruitment_post.api;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.request.CreateRecruitmentPostRequestDto;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.response.CreateRecruitmentPostResponseDto;
+import synk.meeteam.domain.recruitment.recruitment_post.dto.response.GetRecruitmentPostResponseDto;
+import synk.meeteam.domain.user.user.entity.User;
+import synk.meeteam.security.AuthUser;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +23,13 @@ public class RecruitmentPostController implements RecruitmentPostApi {
     @Override
     public ResponseEntity<CreateRecruitmentPostResponseDto> createRecruitmentPost(
             @Valid CreateRecruitmentPostRequestDto requestDto) {
+        return null;
+    }
+
+    @GetMapping
+    @Override
+    public ResponseEntity<GetRecruitmentPostResponseDto> getRecruitmentPost(@Valid @RequestParam Long id,
+                                                                            @AuthUser User user) {
         return null;
     }
 }
