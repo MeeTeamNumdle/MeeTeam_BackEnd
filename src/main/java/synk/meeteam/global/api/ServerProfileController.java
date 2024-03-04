a8a8a98dc71dcd012fe712785ad7660842b779d6
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-03-05 02:25:45 +0900

    [FEAT] 이미지 통신 파이프라인 구현 (#69)
    * [ADD] 테스트를 위한 임시 코드 작성(나중에 실제 개발할 때 한 번 테스트해보고 삭제요망)
    
    * [FEAT] presignedUrl put, get 서비스 로직 작성
    
    * [ADD] 의존성 추가
    
    * [DEL] 더미데이터 삭제
    
    * [ADD] presignedUrl 관련 설정 추가

--- src/main/java/synk/meeteam/global/api/ServerProfileController.java
+++ src/main/java/synk/meeteam/global/api/ServerProfileController.java
@@ -5,14 +5,28 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import synk.meeteam.infra.s3.service.S3Service;
+import synk.meeteam.infra.s3.service.vo.PreSignedUrlVO;
 
 @RestController
 @RequiredArgsConstructor
 public class ServerProfileController {
 
     private final Environment env;
+    private final S3Service s3Service;
 
     @GetMapping("/profile")
     public ResponseEntity<String> getProfile() {
-        return ResponseEntity.ok(env.getActiveProfiles()[0]);}
+        return ResponseEntity.ok(env.getActiveProfiles()[0]);
+    }
+
+    @GetMapping("/presigned-get-test")
+    public ResponseEntity<String> getPreSignedGetUrl() {
+        return ResponseEntity.ok().body(s3Service.createPreSignedGetUrl("test-folder/", "test1.png"));
+    }
+
+    @GetMapping("/presigned-post-test")
+    public ResponseEntity<PreSignedUrlVO> getPreSignedPutUrl() {
+        return ResponseEntity.ok().body(s3Service.getUploadPreSignedUrl("test-folder/"));
+    }
 }
