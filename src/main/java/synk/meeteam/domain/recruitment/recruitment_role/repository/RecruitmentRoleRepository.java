d9a7f12de3138690fbea79d245d637aef8994133
mikekks <thdalsrb79@naver.com>
2024-03-06 02:53:41 +0900

    [FEAT] 신청 가능한 구인 역할 조회 기능 개발

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_role/repository/RecruitmentRoleRepository.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_role/repository/RecruitmentRoleRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import synk.meeteam.domain.recruitment.recruitment_post.entity.RecruitmentPost;
 import synk.meeteam.domain.recruitment.recruitment_role.entity.RecruitmentRole;
 
 public interface RecruitmentRoleRepository extends JpaRepository<RecruitmentRole, Long> {
     @Query("SELECT r FROM RecruitmentRole r JOIN FETCH r.recruitmentRoleSkills s JOIN FETCH s.skill WHERE r.recruitmentPost.id = :postId")
     List<RecruitmentRole> findByPostIdWithSkills(@Param("postId") Long postId);
+
+    List<RecruitmentRole> findAllByRecruitmentPost(RecruitmentPost recruitmentPost);
 }
--- src/main/java/synk/meeteam/domain/recruitment/recruitment_role/service/RecruitmentRoleService.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_role/service/RecruitmentRoleService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import synk.meeteam.domain.recruitment.recruitment_post.entity.RecruitmentPost;
 import synk.meeteam.domain.recruitment.recruitment_role.entity.RecruitmentRole;
 import synk.meeteam.domain.recruitment.recruitment_role.repository.RecruitmentRoleRepository;
 
@@ -19,5 +20,15 @@ public class RecruitmentRoleService {
 
     public List<RecruitmentRole> findByRecruitmentPostId(Long recruitmentPostId) {
         return recruitmentRoleRepository.findByPostIdWithSkills(recruitmentPostId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RecruitmentRole> findAvailableRecruitmentRole(RecruitmentPost recruitmentPost) {
+        List<RecruitmentRole> recruitmentRoles = recruitmentRoleRepository.findAllByRecruitmentPost(
+                recruitmentPost);
+
+        return recruitmentRoles.stream()
+                .filter(recruitmentRole -> recruitmentRole.isAvailableRecruitmentRole())
+                .toList();
     }
 }
