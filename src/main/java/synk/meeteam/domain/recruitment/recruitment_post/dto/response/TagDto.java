989f13bf49064bc0c2f968236fb642bf015572fd
mikekks <thdalsrb79@naver.com>
2024-03-05 21:47:25 +0900

    [FEAT] RecruitmentTag-> dto 변환함수 추가

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/response/TagDto.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/response/TagDto.java
@@ -1,6 +1,7 @@
 package synk.meeteam.domain.recruitment.recruitment_post.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import synk.meeteam.domain.recruitment.recruitment_tag.entity.RecruitmentTag;
 
 @Schema(name = "TagDto", description = "구인태그 Dto")
 public record TagDto(
@@ -9,4 +10,7 @@ public record TagDto(
         @Schema(description = "구인 태그 이름", example = "대학생")
         String name
 ) {
+        public static TagDto from(RecruitmentTag recruitmentTag) {
+                return new TagDto(recruitmentTag.getTag().getId(), recruitmentTag.getTag().getName());
+        }
 }
