7c11dac1811609b94172effe4c3a3e88a60ea529
goder-0 <goder0@naver.com>
2024-03-05 20:31:44 +0900

    [DOCS] 역할, 수업명, 교수명, 태그 자동완성 API 스웨거

---
+++ src/main/java/synk/meeteam/domain/common/role/dto/RoleDto.java
@@ -0,0 +1,13 @@
+package synk.meeteam.domain.common.role.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDto {
+    private Long id;
+    private String name;
+}
