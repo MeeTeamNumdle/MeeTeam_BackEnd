350e21faba57e5d20f5f638f522a7687a29ed5a7
goder <goder0@naver.com>
2024-03-07 13:20:20 +0900

    [DOCS] 유저 프로필 수정 API 스웨거 (#54)
    * [ADD] 유저 프로필에 필요한 엔티티 추가
    - Award
    - SubEmail
    
    * [DEL] 더미데이터 제거
    
    * [DEL] 각 데이터 별 DTO 작성
    
    * [CHORE] 타입 제거 및 description 추가
    
    * [DOCS] 유저 프로필 정보 업데이터 API 스웨거 작성

--- src/main/java/synk/meeteam/domain/user/user/api/UserController.java
+++ src/main/java/synk/meeteam/domain/user/user/api/UserController.java
@@ -1,12 +1,18 @@
 package synk.meeteam.domain.user.user.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import synk.meeteam.domain.user.user.dto.request.UpdateProfileRequestDto;
 import synk.meeteam.domain.user.user.dto.response.CheckDuplicateNicknameResponseDto;
+import synk.meeteam.domain.user.user.entity.User;
 import synk.meeteam.domain.user.user.service.UserService;
+import synk.meeteam.security.AuthUser;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +22,18 @@ public class UserController implements UserApi {
     private final UserService userService;
 
     @Override
+    @PutMapping("/profile")
+    public ResponseEntity<Void> saveProfile(@AuthUser User user, @RequestBody UpdateProfileRequestDto requestDto) {
+        return ResponseEntity.ok().build();
+    }
+
+
+    @Override
     @GetMapping("/search/check-duplicate")
-    public CheckDuplicateNicknameResponseDto checkDuplicateNickname(@RequestParam("nickname") String nickname) {
+    public ResponseEntity<CheckDuplicateNicknameResponseDto> checkDuplicateNickname(
+            @RequestParam("nickname") String nickname) {
         boolean available = userService.checkDuplicateNickname(nickname);
 
-        return CheckDuplicateNicknameResponseDto.of(available);
+        return ResponseEntity.ok(CheckDuplicateNicknameResponseDto.of(available));
     }
 }
