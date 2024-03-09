0c83f52dc4eae164fe40ff0a113b4513ab93bd52
mikekks <thdalsrb79@naver.com>
2024-03-09 23:22:39 +0900

    [FEAT] path variable 예외 처리

--- src/main/java/synk/meeteam/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/synk/meeteam/global/common/exception/GlobalExceptionHandler.java
@@ -10,8 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
@@ -57,6 +59,28 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(exceptionType.httpStatus())
                 .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
     }
+
+    /**
+     * path variable errors
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e) {
+        ExceptionType exceptionType = INVALID_INPUT_VALUE;
+        log.error(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
+    @ExceptionHandler(MissingPathVariableException.class)
+    public ResponseEntity<ExceptionResponse> handleMissingPathVariableException(
+            MissingPathVariableException e) {
+        ExceptionType exceptionType = INVALID_INPUT_VALUE;
+        log.error(String.format(LOG_FORMAT, e.getMessage()), e);
+        return ResponseEntity.status(exceptionType.httpStatus())
+                .body(ExceptionResponse.of(exceptionType.name(), exceptionType.message()));
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception e) {
