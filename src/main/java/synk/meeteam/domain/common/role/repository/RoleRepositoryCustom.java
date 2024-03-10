89e4a27aae630d4b5ca7ec7cd0b2f2ee8500da97
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-03-10 15:43:05 +0900

    [FEAT] 신청 정보 조회 API (#90)
    * [CHORE] 페치타입 변경
    
    * [DEL] 사용하지 않은 코드 제거
    
    * [DOCS] Dto 설명 추가
    
    * [CHORE] 로직 수정으로 인한 Dto 변경
    
    * [CHORE] 요청 url 수정으로 인한 변경
    
    * [FEAT] path variable 예외 처리
    
    * [FEAT] 구인 신청 정보 조회 컨트롤러 개발 및 테스트
    
    * [FEAT] 신청가능역할 조회 기능 개발 및 테스트
    
    * [FEAT] 신청가능역할 조회 기능 서비스단 개발 및 테스트
    
    * [DOCS] description 변경
    
    * [REFACTOR] 반복되는 코드 개선

--- src/main/java/synk/meeteam/domain/common/role/repository/RoleRepositoryCustom.java
+++ src/main/java/synk/meeteam/domain/common/role/repository/RoleRepositoryCustom.java
@@ -4,5 +4,7 @@ import java.util.List;
 import synk.meeteam.domain.common.role.dto.RoleDto;
 
 public interface RoleRepositoryCustom {
+    List<RoleDto> findAvailableRoleByRecruitmentId(Long postId);
+
     List<RoleDto> findAllByKeywordAndTopLimit(String keyword, long limit);
 }
--- src/main/java/synk/meeteam/domain/common/role/repository/RoleRepositoryCustomImpl.java
+++ src/main/java/synk/meeteam/domain/common/role/repository/RoleRepositoryCustomImpl.java
@@ -1,7 +1,9 @@
 package synk.meeteam.domain.common.role.repository;
 
 import static synk.meeteam.domain.common.role.entity.QRole.role;
+import static synk.meeteam.domain.recruitment.recruitment_role.entity.QRecruitmentRole.recruitmentRole;
 
+import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,19 @@ import synk.meeteam.domain.common.role.dto.RoleDto;
 @RequiredArgsConstructor
 public class RoleRepositoryCustomImpl implements RoleRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<RoleDto> findAvailableRoleByRecruitmentId(Long postId) {
+        Predicate predicate = recruitmentRole.count.subtract(recruitmentRole.recruitedCount).gt(0);
+
+        return queryFactory
+                .select(new QRoleDto(role.id, role.name))
+                .from(role)
+                .leftJoin(recruitmentRole)
+                .on(role.id.eq(recruitmentRole.role.id))
+                .where(recruitmentRole.recruitmentPost.id.eq(postId), predicate)
+                .fetch();
+    }
 
     @Override
     public List<RoleDto> findAllByKeywordAndTopLimit(String keyword, long limit) {
