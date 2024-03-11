04d72c1431c59baf0ca208b83b40814d39edd1a8
mikekks <thdalsrb79@naver.com>
2024-03-11 16:57:05 +0900

    [CHORE] 변수명 및 클래스 이름 변경

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/request/ApplyRecruitmentRequestDto.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/request/ApplyRecruitmentRequestDto.java
@@ -2,12 +2,10 @@ package synk.meeteam.domain.recruitment.recruitment_post.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(name = "applyRecruitmentRequestDto", description = "구인 신청 Dto")
-public record applyRecruitmentRequestDto(
-        @Schema(description = "구인글 id", example = "1")
-        Long postId,
+@Schema(name = "ApplyRecruitmentRequestDto", description = "구인 신청 Dto")
+public record ApplyRecruitmentRequestDto(
         @Schema(description = "신청 역할 id", example = "1")
-        Long applicationRoleId,
+        Long applyRoleId,
         @Schema(description = "전하는 말", example = "저 관심있어용")
         String message
 ) {
