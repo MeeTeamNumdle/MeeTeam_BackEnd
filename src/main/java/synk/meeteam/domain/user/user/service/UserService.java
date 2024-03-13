816bf01caf6eb86f5a8042ff0fc909f75f19d7be
goder-0 <goder0@naver.com>
2024-03-13 11:10:54 +0900

    [ADD] UserService 의 유저 프로필 일부 수정 기능 추가

--- src/main/java/synk/meeteam/domain/user/user/service/UserService.java
+++ src/main/java/synk/meeteam/domain/user/user/service/UserService.java
@@ -1,9 +1,15 @@
 package synk.meeteam.domain.user.user.service;
 
+import static synk.meeteam.domain.user.user.exception.UserExceptionType.DUPLICATE_NICKNAME;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import synk.meeteam.domain.common.role.entity.Role;
+import synk.meeteam.domain.common.role.repository.RoleRepository;
+import synk.meeteam.domain.user.user.dto.command.UpdateProfileCommand;
 import synk.meeteam.domain.user.user.entity.User;
+import synk.meeteam.domain.user.user.exception.UserException;
 import synk.meeteam.domain.user.user.repository.UserRepository;
 
 @Service
@@ -11,13 +17,44 @@ import synk.meeteam.domain.user.user.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Transactional(readOnly = true)
-    public boolean checkDuplicateNickname(String nickname) {
+    public boolean checkAvailableNickname(String nickname) {
         User foundUser = userRepository.findByNickname(nickname).orElse(null);
-        if (foundUser != null) {
-            return false;
+        return foundUser == null;
+    }
+
+    @Transactional
+    public void updateNickname(User user, String newNickname) {
+        if (user.isNotEqualNickname(newNickname)) {
+            if (checkAvailableNickname(newNickname)) {
+                user.updateNickname(newNickname);
+            } else {
+                throw new UserException(DUPLICATE_NICKNAME);
+            }
         }
-        return true;
+    }
+
+    @Transactional
+    public void updateProfile(User user, UpdateProfileCommand command) {
+
+        Role interestRole = roleRepository.findByIdOrElseThrowException(command.interest_id());
+        user.updateProfile(
+                command.name(),
+                command.pictureUrl(),
+                command.subEmail(),
+                command.isPublicSubEmail(),
+                command.isPublicSchoolEmail(),
+                command.isSchoolMain(),
+                command.phoneNumber(),
+                command.isPublicPhone(),
+                command.oneLineIntroduction(),
+                command.mainIntroduction(),
+                command.gpa(),
+                command.maxGpa(),
+                interestRole
+        );
+        userRepository.save(user);
     }
 }
