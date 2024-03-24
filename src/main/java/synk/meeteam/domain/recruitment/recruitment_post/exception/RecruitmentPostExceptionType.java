3675e128d1df24874be5881fdc45107275ff09f0
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-03-24 15:41:05 +0900

    [FEAT] 구인마감 구현 (#100)
    * [DOCS] 구인마감 API 스웨거 추가
    
    * [FEAT] 구인마감 서비스 로직 구현 및 테스트
    
    * [FEAT] 구인글 작성자 검증 로직 구현 및 테스트
    
    * [FEAT] 구인 마감 컨트롤러 로직 구현
    
    * [REFACTOR] 작성자 검증 로직 객체 내부로 수정

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/exception/RecruitmentPostExceptionType.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/exception/RecruitmentPostExceptionType.java
@@ -6,6 +6,7 @@ import synk.meeteam.global.common.exception.ExceptionType;
 
 @RequiredArgsConstructor
 public enum RecruitmentPostExceptionType implements ExceptionType {
+    INVALID_USER_ID(HttpStatus.BAD_REQUEST, "접근할 수 없는 유저입니다."),
     NOT_FOUND_POST(HttpStatus.NOT_FOUND, "유효하지 않은 게시글 id 입니다.");
 
     private final HttpStatus status;
