4b0331cdd831b738913100867e325788a61fc47b
goder-0 <goder0@naver.com>
2024-03-26 01:45:44 +0900

    [ADD] 유저스킬조회 Repository 추가 및 테스트

--- src/main/java/synk/meeteam/domain/user/user_skill/repository/UserSkillRepository.java
+++ src/main/java/synk/meeteam/domain/user/user_skill/repository/UserSkillRepository.java
@@ -3,7 +3,7 @@ package synk.meeteam.domain.user.user_skill.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import synk.meeteam.domain.user.user_skill.entity.UserSkill;
 
-public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
+public interface UserSkillRepository extends JpaRepository<UserSkill, Long>, UserSkillCustomRepository {
 
     void deleteAllByCreatedBy(Long id);
 }
