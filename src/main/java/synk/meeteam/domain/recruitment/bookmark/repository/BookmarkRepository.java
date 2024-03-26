02e59210b814bc7eea119cc0cd41f614e4da0b8c
mikekks <thdalsrb79@naver.com>
2024-03-27 01:08:37 +0900

    [FEAT] 북마크 조회 로직 레포지토리단 구현

---
+++ src/main/java/synk/meeteam/domain/recruitment/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,11 @@
+package synk.meeteam.domain.recruitment.bookmark.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import synk.meeteam.domain.recruitment.bookmark.entity.Bookmark;
+import synk.meeteam.domain.recruitment.recruitment_post.entity.RecruitmentPost;
+import synk.meeteam.domain.user.user.entity.User;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    Optional<Bookmark> findByRecruitmentPostAndUser(RecruitmentPost recruitmentPost, User user);
+}
