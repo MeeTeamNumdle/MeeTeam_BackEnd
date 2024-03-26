4757acfdd7cb9c5eee1f3da34da8a9a27ccd7860
goder-0 <goder0@naver.com>
2024-03-27 00:06:01 +0900

    [ADD] UserController의 Test를 위한 Fixture 추가

--- src/test/java/synk/meeteam/domain/user/user/UserFixture.java
+++ src/test/java/synk/meeteam/domain/user/user/UserFixture.java
@@ -4,12 +4,19 @@ package synk.meeteam.domain.user.user;
 import java.time.LocalDate;
 import java.util.List;
 import synk.meeteam.domain.common.department.entity.Department;
+import synk.meeteam.domain.common.skill.dto.SkillDto;
 import synk.meeteam.domain.common.university.entity.University;
+import synk.meeteam.domain.portfolio.portfolio.dto.GetProfilePortfolioDto;
+import synk.meeteam.domain.user.award.dto.GetProfileAwardDto;
 import synk.meeteam.domain.user.award.dto.UpdateAwardDto;
 import synk.meeteam.domain.user.user.dto.request.UpdateProfileRequestDto;
+import synk.meeteam.domain.user.user.dto.response.GetProfileEmailDto;
+import synk.meeteam.domain.user.user.dto.response.GetProfilePhoneDto;
+import synk.meeteam.domain.user.user.dto.response.GetProfileResponseDto;
 import synk.meeteam.domain.user.user.entity.User;
 import synk.meeteam.domain.user.user.entity.enums.Authority;
 import synk.meeteam.domain.user.user.entity.enums.PlatformType;
+import synk.meeteam.domain.user.user_link.dto.GetProfileUserLinkDto;
 import synk.meeteam.domain.user.user_link.dto.UpdateUserLinkDto;
 
 public class UserFixture {
@@ -53,6 +60,38 @@ public class UserFixture {
                 List.of(new UpdateAwardDto("공공데이터공모전", "장려상수상", LocalDate.parse("2023-02-02"),
                         LocalDate.parse("2023-03-01"))),
                 List.of(1L, 2L)
+        );
+    }
+
+    public static GetProfileResponseDto createReadProfile() {
+        return new GetProfileResponseDto(
+                "https://dasfsdf.png",
+                "민지",
+                "mingi123",
+                true,
+                "백엔드 개발자",
+                "한줄소개입니다.",
+                "<h1>자기소개입니다.</h1></br>안녕하세요. 저는 백엔드개발을 합니다.",
+                new GetProfileEmailDto("minji@kw.ac.kr", true, true),
+                new GetProfileEmailDto("mingi@naver.com", true, false),
+                new GetProfilePhoneDto("010-1234-5678", true),
+                "광운대학교",
+                "소프트웨어학부",
+                4.5,
+                4.3,
+                2019,
+                List.of(
+                        new GetProfilePortfolioDto(1L, "Meeteam 팀을 만나다", "https://~",
+                                LocalDate.of(2023, 6, 11), LocalDate.of(2024, 3, 25), "개발", "백엔드개발자")
+                ),
+                List.of(
+                        new GetProfileUserLinkDto("https://~~", "Link"),
+                        new GetProfileUserLinkDto("https://~~", "Github")
+                ),
+                List.of(new GetProfileAwardDto(
+                        "공공데이터 공모전", "2024-03-24", "2024-03-25", "장려상 수상"
+                )),
+                List.of(new SkillDto(1L, "스프링"), new SkillDto(2L, "깃"))
         );
     }
 
