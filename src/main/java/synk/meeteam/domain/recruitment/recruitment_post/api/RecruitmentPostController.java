aa797765d813c2de42ea832c10e42a695d27d3df
mikekks <thdalsrb79@naver.com>
2024-03-27 02:50:15 +0900

    [DOCS] 구인글 취소 API 스웨거 추가

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/api/RecruitmentPostController.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/api/RecruitmentPostController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -176,6 +177,14 @@ public class RecruitmentPostController implements RecruitmentPostApi {
     @PostMapping("/{id}/bookmark")
     @Override
     public ResponseEntity<Void> bookmarkRecruitmentPost(@Valid @PathVariable("id") Long postId, @AuthUser User user) {
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}/bookmark")
+    @Override
+    public ResponseEntity<Void> cancelBookmarkRecruitmentPost(@Valid @PathVariable("id") Long postId,
+                                                              @AuthUser User user) {
 
         return ResponseEntity.ok().build();
     }
