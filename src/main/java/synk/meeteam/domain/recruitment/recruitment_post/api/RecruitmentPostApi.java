2f6918d4536877113015526d68aa2cca88d67338
goder <goder0@naver.com>
2024-03-28 00:52:48 +0900

    [DOCS] 구인글 목록 검색 조회 API 스웨거 작성 (#144)
    * [DOCS] Dto 추가
    
    * [DOCS] 구인글 목록 검색 조회 API 스웨거 작성

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/api/RecruitmentPostApi.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/api/RecruitmentPostApi.java
@@ -10,15 +10,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.request.ApplyRecruitmentRequestDto;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.request.CreateRecruitmentPostRequestDto;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.request.SetOpenKakaoLinkRequestDto;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.response.CreateRecruitmentPostResponseDto;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.response.GetApplyInfoResponseDto;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.response.GetRecruitmentPostResponseDto;
+import synk.meeteam.domain.recruitment.recruitment_post.dto.response.PaginationSearchPostResponseDto;
 import synk.meeteam.domain.user.user.entity.User;
 import synk.meeteam.security.AuthUser;
 
@@ -114,6 +117,24 @@ public interface RecruitmentPostApi {
     )
     @Operation(summary = "구인글 북마크 취소 API")
     ResponseEntity<Void> cancelBookmarkRecruitmentPost(@PathVariable("id") Long postId, @AuthUser User user);
+
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "검색 성공"),
+                    @ApiResponse(responseCode = "403", description = "접근 권한이 없습니다")
+            }
+    )
+    @Operation(summary = "구인글 목록 검색 API")
+    ResponseEntity<PaginationSearchPostResponseDto> searchRecruitmentPost(
+            @RequestParam Long fieldId,
+            @RequestParam Long scopeIndex,
+            @RequestParam Long categoryIndex,
+            @RequestParam List<Long> skills,
+            @RequestParam List<Long> roles,
+            @RequestParam List<Long> tags,
+            @RequestParam String keyword
+    );
+
 
     @ApiResponses(
             value = {
