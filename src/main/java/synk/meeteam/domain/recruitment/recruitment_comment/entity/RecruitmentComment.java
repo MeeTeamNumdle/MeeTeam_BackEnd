fa102cdd32e268f6d15384df2f8f9abdc8ea9f7b
mikekks <thdalsrb79@naver.com>
2024-03-29 15:00:59 +0900

    [DEL] setter 삭제

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_comment/entity/RecruitmentComment.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_comment/entity/RecruitmentComment.java
@@ -15,13 +15,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import synk.meeteam.domain.recruitment.recruitment_post.entity.RecruitmentPost;
 import synk.meeteam.global.entity.BaseEntity;
 
 @Getter
-@Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RecruitmentComment extends BaseEntity {
