d6efa2be675f005930d57462545bbf07ea4b2e05
mikekks <thdalsrb79@naver.com>
2024-03-29 16:02:01 +0900

    [FEAT] 댓글 저장 서비스 로직 구현 및 테스트

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_comment/service/RecruitmentCommentService.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_comment/service/RecruitmentCommentService.java
@@ -1,9 +1,14 @@
 package synk.meeteam.domain.recruitment.recruitment_comment.service;
 
+import static synk.meeteam.domain.recruitment.recruitment_comment.exception.RecruitmentCommentExceptionType.INVALID_COMMENT;
+
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import synk.meeteam.domain.recruitment.recruitment_comment.entity.RecruitmentComment;
+import synk.meeteam.domain.recruitment.recruitment_comment.exception.RecruitmentCommentException;
 import synk.meeteam.domain.recruitment.recruitment_comment.repository.RecruitmentCommentRepository;
 import synk.meeteam.domain.recruitment.recruitment_comment.service.vo.RecruitmentCommentVO;
 import synk.meeteam.domain.recruitment.recruitment_post.dto.response.GetCommentResponseDto;
@@ -50,5 +55,40 @@ public class RecruitmentCommentService {
         }
 
         return groupedComments;
+    }
+
+    @Transactional
+    public RecruitmentComment registerRecruitmentComment(RecruitmentComment recruitmentComment) {
+        validateComment(recruitmentComment);
+
+        return recruitmentCommentRepository.save(recruitmentComment);
+    }
+
+    private void validateComment(RecruitmentComment recruitmentComment) {
+        long nextGroupNumber = 1;
+        long nextGroupOrder = 1;
+
+        if (recruitmentComment.isParent()) {  // 페치 조인으로 변경하는게 좋을듯?
+            // 댓글인 경우
+            // 현재 가장 최신 그룹 번호를 찾아서, 그 다음 번호로 할당 + 그룹오더 1로 할당
+            RecruitmentComment foundRecruitmentComment = recruitmentCommentRepository.findFirstByRecruitmentPostOrderByGroupNumberDesc(
+                    recruitmentComment.getRecruitmentPost()).orElse(null);
+            if (foundRecruitmentComment != null) {
+                nextGroupNumber = foundRecruitmentComment.getGroupNumber() + 1;
+            }
+
+        } else if (!recruitmentComment.isParent()) {
+            // 대댓글인 경우
+            // 해당 그룹 번호가 있는지 찾아서, 그 그룹 오더 가장 최신 번호 찾아서, 그 다음 번호로 할당
+            RecruitmentComment foundRecruitmentComment = recruitmentCommentRepository.findLatestGroupOrderOrElseThrow(
+                    recruitmentComment.getRecruitmentPost(), recruitmentComment.getGroupNumber());
+
+            nextGroupOrder = foundRecruitmentComment.getGroupOrder() + 1;
+        }
+
+        if (nextGroupNumber != recruitmentComment.getGroupNumber()
+                || nextGroupOrder != recruitmentComment.getGroupOrder()) {
+            throw new RecruitmentCommentException(INVALID_COMMENT);
+        }
     }
 }
--- src/test/java/synk/meeteam/domain/recruitment/recruitment_comment/RecruitmentCommentServiceTest.java
+++ src/test/java/synk/meeteam/domain/recruitment/recruitment_comment/RecruitmentCommentServiceTest.java
@@ -1,14 +1,21 @@
 package synk.meeteam.domain.recruitment.recruitment_comment;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static synk.meeteam.domain.recruitment.recruitment_comment.exception.RecruitmentCommentExceptionType.INVALID_COMMENT;
 
 import java.util.List;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import synk.meeteam.domain.recruitment.recruitment_comment.entity.RecruitmentComment;
+import synk.meeteam.domain.recruitment.recruitment_comment.exception.RecruitmentCommentException;
 import synk.meeteam.domain.recruitment.recruitment_comment.repository.RecruitmentCommentRepository;
 import synk.meeteam.domain.recruitment.recruitment_comment.service.RecruitmentCommentService;
 import synk.meeteam.domain.recruitment.recruitment_comment.service.vo.RecruitmentCommentVO;
@@ -57,4 +64,203 @@ public class RecruitmentCommentServiceTest {
                 .containsExactly("닉네임입니다3", false);
 
     }
+
+    @Test
+    void 댓글등록_성공_최초댓글경우() {
+        // given
+        RecruitmentPost recruitmentPost = RecruitmentPostFixture.createRecruitmentPost("제목입니당");
+        long groupNumber = 1L;
+        long groupOrder = 1L;
+
+        RecruitmentComment comment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(true)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(groupNumber)
+                .groupOrder(groupOrder)
+                .build();
+
+        doReturn(comment).when(recruitmentCommentRepository).save(any());
+        doReturn(Optional.ofNullable(null)).when(recruitmentCommentRepository)
+                .findFirstByRecruitmentPostOrderByGroupNumberDesc(recruitmentPost);
+
+        // when
+        RecruitmentComment savedRecruitmentComment = recruitmentCommentService.registerRecruitmentComment(comment);
+
+        // then
+        Assertions.assertThat(savedRecruitmentComment)
+                .extracting("content", "isParent", "groupNumber", "groupOrder")
+                .containsExactly("저 하고 싶어요", true, groupNumber, groupOrder);
+    }
+
+    @Test
+    void 댓글등록_성공_이전댓글있는경우() {
+        // given
+        RecruitmentPost recruitmentPost = RecruitmentPostFixture.createRecruitmentPost("제목입니당");
+        long prevGroupNumber = 1L;
+        long prevGroupOrder = 1L;
+
+        long groupNumber = 2L;
+        long groupOrder = 1L;
+
+        RecruitmentComment prevComment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(true)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(prevGroupNumber)
+                .groupOrder(prevGroupOrder)
+                .build();
+
+        RecruitmentComment comment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(true)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(groupNumber)
+                .groupOrder(groupOrder)
+                .build();
+
+        doReturn(comment).when(recruitmentCommentRepository).save(any());
+        doReturn(Optional.ofNullable(prevComment)).when(recruitmentCommentRepository)
+                .findFirstByRecruitmentPostOrderByGroupNumberDesc(recruitmentPost);
+
+        // when
+        RecruitmentComment savedRecruitmentComment = recruitmentCommentService.registerRecruitmentComment(comment);
+
+        // then
+        Assertions.assertThat(savedRecruitmentComment)
+                .extracting("content", "isParent", "groupNumber", "groupOrder")
+                .containsExactly("저 하고 싶어요", true, groupNumber, groupOrder);
+    }
+
+    @Test
+    void 대댓글등록_성공() {
+        // given
+        RecruitmentPost recruitmentPost = RecruitmentPostFixture.createRecruitmentPost("제목입니당");
+        long parentGroupNumber = 1L;
+        long parentGroupOrder = 1L;
+
+        long groupNumber = 1L;
+        long groupOrder = 2L;
+
+        RecruitmentComment parentComment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(true)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(parentGroupNumber)
+                .groupOrder(parentGroupOrder)
+                .build();
+
+        RecruitmentComment childComment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(false)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(groupNumber)
+                .groupOrder(groupOrder)
+                .build();
+
+        doReturn(childComment).when(recruitmentCommentRepository).save(any());
+        doReturn(parentComment).when(recruitmentCommentRepository)
+                .findLatestGroupOrderOrElseThrow(recruitmentPost, groupNumber);
+
+        // when
+        RecruitmentComment savedRecruitmentComment = recruitmentCommentService.registerRecruitmentComment(childComment);
+
+        // then
+        Assertions.assertThat(savedRecruitmentComment)
+                .extracting("content", "isParent", "groupNumber", "groupOrder")
+                .containsExactly("저 하고 싶어요", false, groupNumber, groupOrder);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,1", "1,2", "1,0", "2,1", "2,2"})
+    void 댓글등록_예외발생_댓글의groupNumber나groupOrder가잘못된경우(long groupNumber, long groupOrder) {
+        // given
+        RecruitmentPost recruitmentPost = RecruitmentPostFixture.createRecruitmentPost("제목입니당");
+
+        RecruitmentComment comment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(true)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(groupNumber)
+                .groupOrder(groupOrder)
+                .build();
+
+        doReturn(Optional.ofNullable(null)).when(recruitmentCommentRepository)
+                .findFirstByRecruitmentPostOrderByGroupNumberDesc(recruitmentPost);
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> recruitmentCommentService.registerRecruitmentComment(comment))
+                .isInstanceOf(RecruitmentCommentException.class)
+                .hasMessageContaining(INVALID_COMMENT.message());
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"2,1", "2,4", "2,0"})
+    void 대댓글등록_예외발생_대댓글의groupNumber나groupOrder가잘못된경우(long groupNumber, long groupOrder) {
+        // given
+        RecruitmentPost recruitmentPost = RecruitmentPostFixture.createRecruitmentPost("제목입니당");
+        long parentGroupNumber = 2L;
+        long parentGroupOrder = 2L;
+
+        RecruitmentComment parentComment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(true)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(parentGroupNumber)
+                .groupOrder(parentGroupOrder)
+                .build();
+
+        RecruitmentComment childComment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(false)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(groupNumber)
+                .groupOrder(groupOrder)
+                .build();
+
+        doReturn(parentComment).when(recruitmentCommentRepository)
+                .findLatestGroupOrderOrElseThrow(recruitmentPost, groupNumber);
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> recruitmentCommentService.registerRecruitmentComment(childComment))
+                .isInstanceOf(RecruitmentCommentException.class)
+                .hasMessageContaining(INVALID_COMMENT.message());
+    }
+
+    @Test
+    void 대댓글등록_예외발생_존재하지않은groupNumer경우() {
+        // given
+        RecruitmentPost recruitmentPost = RecruitmentPostFixture.createRecruitmentPost("제목입니당");
+        long parentGroupNumber = 2L;
+        long parentGroupOrder = 2L;
+
+        long groupNumber = 5L;
+        long groupOrder = 2L;
+
+        RecruitmentComment parentComment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(true)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(parentGroupNumber)
+                .groupOrder(parentGroupOrder)
+                .build();
+
+        RecruitmentComment childComment = RecruitmentComment.builder()
+                .content("저 하고 싶어요")
+                .isParent(false)
+                .recruitmentPost(recruitmentPost)
+                .groupNumber(groupNumber)
+                .groupOrder(groupOrder)
+                .build();
+
+        doReturn(parentComment).when(recruitmentCommentRepository)
+                .findLatestGroupOrderOrElseThrow(recruitmentPost, groupNumber);
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> recruitmentCommentService.registerRecruitmentComment(childComment))
+                .isInstanceOf(RecruitmentCommentException.class)
+                .hasMessageContaining(INVALID_COMMENT.message());
+    }
+
 }
