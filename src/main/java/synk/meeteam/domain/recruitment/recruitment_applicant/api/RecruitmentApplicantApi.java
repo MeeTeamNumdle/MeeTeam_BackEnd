1a184b80a08a1f20008bc09bb974008b81331320
mikekks <thdalsrb79@naver.com>
2024-03-31 01:51:31 +0900

    [DOCS] 오픈카톡방 링크 설정 API 스웨거 PR

---
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/api/RecruitmentApplicantApi.java
@@ -0,0 +1,23 @@
+package synk.meeteam.domain.recruitment.recruitment_applicant.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import synk.meeteam.domain.recruitment.recruitment_applicant.dto.request.SetLinkRequestDto;
+
+@Tag(name = "applicant", description = "신청자 관리 관련 API")
+public interface RecruitmentApplicantApi {
+
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "오픈카톡방 링크 설정 성공"),
+            }
+    )
+    @Operation(summary = "오픈카톡 링크 설정 API")
+    ResponseEntity<Void> setLink(@PathVariable("id") Long postId, @Valid @RequestBody SetLinkRequestDto requestDto);
+}
