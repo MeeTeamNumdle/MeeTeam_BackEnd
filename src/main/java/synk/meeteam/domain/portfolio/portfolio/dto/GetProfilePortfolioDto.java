7c5e9e80925cb849b67f8ebfe34f4ccfa7eefb85
goder <goder0@naver.com>
2024-04-02 16:22:41 +0900

    [CHORE] UserPortfolio 조회 API 스웨거 작성 및 조회 ResponseDto 수정 (#133)
    * [DOCS] UserPortfolio 조회 API 스웨거 추가
    
    * [CHORE] 포트폴리오 핀 여부, 핀 순서 추가 및 기간 제거
    
    * [CHORE] 타입 변경에 따른 인자 수정

--- src/main/java/synk/meeteam/domain/portfolio/portfolio/dto/GetProfilePortfolioDto.java
+++ src/main/java/synk/meeteam/domain/portfolio/portfolio/dto/GetProfilePortfolioDto.java
@@ -1,7 +1,6 @@
 package synk.meeteam.domain.portfolio.portfolio.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDate;
 
 public record GetProfilePortfolioDto(
         @Schema(description = "포트폴리오 id", example = "1")
@@ -10,13 +9,13 @@ public record GetProfilePortfolioDto(
         String title,
         @Schema(description = "메인 커버이미지 url", example = "https://~~")
         String mainImageUrl,
-        @Schema(description = "시작일")
-        LocalDate startDate,
-        @Schema(description = "종료일")
-        LocalDate endDate,
         @Schema(description = "분야", example = "개발")
         String field,
         @Schema(description = "역할", example = "백엔드 개발자")
-        String role
+        String role,
+        @Schema(description = "핀여부", example = "true")
+        Boolean isPinned,
+        @Schema(description = "핀순서", example = "1")
+        Long pinOrder
 ) {
 }
