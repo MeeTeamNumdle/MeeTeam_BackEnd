25f8e091c46401c9cd46dc95e2574c491c576e44
mikekks <thdalsrb79@naver.com>
2024-04-02 18:30:00 +0900

    [CHORE] 댓글 id로 Dto 수정

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/request/DeleteCommentRequestDto.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/dto/request/DeleteCommentRequestDto.java
@@ -4,9 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "DeleteCommentRequestDto", description = "댓글 삭제 요청 Dto")
 public record DeleteCommentRequestDto(
-        @Schema(description = "댓글 그룹 번호", example = "2")
-        long groupNumber,
-        @Schema(description = "특정 댓글 그룹 내 순서", example = "3")
-        long groupOrder
+        @Schema(description = "댓글 id", example = "2")
+        long commentId
 ) {
 }
