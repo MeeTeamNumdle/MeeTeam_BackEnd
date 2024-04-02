1aad7d7d6040e6f814ca3f7ffece622e2f10e328
mikekks <thdalsrb79@naver.com>
2024-04-02 14:36:03 +0900

    [FEAT] 링크 설정 컨트롤러 로직 구현

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/api/RecruitmentApplicantController.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/api/RecruitmentApplicantController.java
@@ -10,16 +10,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import synk.meeteam.domain.recruitment.recruitment_applicant.dto.request.SetLinkRequestDto;
+import synk.meeteam.domain.recruitment.recruitment_post.service.RecruitmentPostService;
+import synk.meeteam.domain.user.user.entity.User;
+import synk.meeteam.security.AuthUser;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/recruitment/applicant")
 public class RecruitmentApplicantController implements RecruitmentApplicantApi {
 
-    @PutMapping("{id}/link")
+    private final RecruitmentPostService recruitmentPostService;
+
+    @PutMapping("/{id}/link")
     @Override
     public ResponseEntity<Void> setLink(@PathVariable("id") Long postId,
-                                        @Valid @RequestBody SetLinkRequestDto requestDto) {
+                                        @Valid @RequestBody SetLinkRequestDto requestDto, @AuthUser User user) {
+
+        recruitmentPostService.setLink(postId, requestDto.link(), user.getId());
 
         return ResponseEntity.ok().build();
     }
