ba6eeb2ef7a536d78ffef470ab7db68fa50afc72
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-04-02 17:01:43 +0900

    [FEAT] 링크 설정 API (#167)
    * [DEL] 구인글 컨트롤러 쪽의 오픈카톡방 링크 설정 API 삭제
    
    * [DEL] 더미데이터 삭제
    
    * [ADD] 예외 클래스, 타입 추가
    
    * [FEAT] 링크 설정 엔티티 로직 구현 및 테스트
    
    * [REFACTOR] 작성자 검증 로직 엔티티 내부로 수정 및 테스트 수정
    
    * [FEAT] 링크 설정 서비스 로직 구현 및 테스트
    
    * [FEAT] 링크 설정 컨트롤러 로직 구현
    
    * [FEAT] 링크 설정 API 통합테스트 진행
    
    * [DOCS] 링크 설정 API 스웨거 수정

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/api/RecruitmentApplicantApi.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/api/RecruitmentApplicantApi.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import synk.meeteam.domain.recruitment.recruitment_applicant.dto.request.SetLinkRequestDto;
+import synk.meeteam.domain.user.user.entity.User;
+import synk.meeteam.security.AuthUser;
 
 @Tag(name = "applicant", description = "신청자 관리 관련 API")
 public interface RecruitmentApplicantApi {
@@ -18,6 +20,7 @@ public interface RecruitmentApplicantApi {
                     @ApiResponse(responseCode = "200", description = "오픈카톡방 링크 설정 성공"),
             }
     )
-    @Operation(summary = "오픈카톡 링크 설정 API")
-    ResponseEntity<Void> setLink(@PathVariable("id") Long postId, @Valid @RequestBody SetLinkRequestDto requestDto);
+    @Operation(summary = "오픈카톡 링크 설정(수정) API")
+    ResponseEntity<Void> setLink(@PathVariable("id") Long postId, @Valid @RequestBody SetLinkRequestDto requestDto,
+                                 @AuthUser User user);
 }
