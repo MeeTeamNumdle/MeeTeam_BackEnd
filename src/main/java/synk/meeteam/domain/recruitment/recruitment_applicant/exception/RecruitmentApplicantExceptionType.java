435e03df6f6b6d38e0ee2c1188f66d6b554b695b
mikekks <thdalsrb79@naver.com>
2024-04-04 14:17:28 +0900

    [ADD] 예외 enum 추가

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/exception/RecruitmentApplicantExceptionType.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/exception/RecruitmentApplicantExceptionType.java
@@ -6,7 +6,8 @@ import synk.meeteam.global.common.exception.ExceptionType;
 
 @RequiredArgsConstructor
 public enum RecruitmentApplicantExceptionType implements ExceptionType {
-    ;
+    INVALID_USER(HttpStatus.BAD_REQUEST, "신청자 정보에 접근할 수 없습니다."),
+    ALREADY_PROCESSED_APPLICANT(HttpStatus.BAD_REQUEST, "이미 신청/거절된 신청자입니다.");
 
     private final HttpStatus status;
     private final String message;
