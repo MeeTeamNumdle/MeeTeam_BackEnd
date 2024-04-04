d98c76a1f2be73cdaaaf22f5469bb5e10a00197d
mikekks <thdalsrb79@naver.com>
2024-04-04 15:29:51 +0900

    [REFACTOR] 신청자 승인 파사드 로직 개선

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/facade/RecruitmentApplicantFacade.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/facade/RecruitmentApplicantFacade.java
@@ -1,6 +1,5 @@
 package synk.meeteam.domain.recruitment.recruitment_applicant.facade;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import synk.meeteam.domain.recruitment.recruitment_applicant.entity.RecruitmentApplicant;
 import synk.meeteam.domain.recruitment.recruitment_applicant.service.RecruitmentApplicantService;
 import synk.meeteam.domain.recruitment.recruitment_post.service.RecruitmentPostService;
-import synk.meeteam.domain.recruitment.recruitment_role.entity.RecruitmentRole;
 import synk.meeteam.domain.recruitment.recruitment_role.service.RecruitmentRoleService;
 
 @Service
@@ -24,27 +22,12 @@ public class RecruitmentApplicantFacade {
     public void approveApplicant(Long postId, Long userId, List<Long> applicantIds) {
 
         List<RecruitmentApplicant> applicants = recruitmentApplicantService.getAllApplicants(applicantIds);
+        recruitmentApplicantService.approveApplicants(applicants, applicantIds, userId);
 
-        recruitmentApplicantService.approveApplicants(applicantIds, applicants, userId);
+        List<Long> roleIds = recruitmentApplicantService.getRoleIds(applicants);
+        Map<Long, Long> recruitedCounts = recruitmentApplicantService.getRecruitedCounts(applicants);
+
         recruitmentPostService.incrementResponseCount(postId, userId, applicantIds.size());
-
-        Map<Long, Long> recruitedRoles = new HashMap<>();
-        List<Long> roleIds = applicants.stream()
-                .map(applicant -> {
-                    recruitedRoles.putIfAbsent(applicant.getRole().getId(), 0L);
-
-                    recruitedRoles.put(applicant.getRole().getId(),
-                            recruitedRoles.get(applicant.getRole().getId()) + 1);
-                    return applicant.getRole().getId();
-                }).toList();
-
-        List<RecruitmentRole> recruitmentRoles = recruitmentRoleService.findAllByRecruitmentAndRoles(postId,
-                roleIds);
-
-        recruitmentRoles.stream()
-                .forEach(recruitmentRole -> {
-                    long recruitedCount = recruitedRoles.get(recruitmentRole.getRole().getId());
-                    recruitmentRole.incrementRecruitedCount(recruitedCount);
-                });
+        recruitmentRoleService.incrementRecruitedCount(postId, roleIds, recruitedCounts);
     }
 }
