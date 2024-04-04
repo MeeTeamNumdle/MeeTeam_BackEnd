c57ba12232d671998134a135d88a0c831431528f
mikekks <thdalsrb79@naver.com>
2024-04-04 14:56:28 +0900

    [FEAT] 1. 신청자 승인 벌크 연산 서비스 로직 구현 2. 검증 로직 구현 3. 신청자 조회 페치조인 구현

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/service/RecruitmentApplicantService.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/service/RecruitmentApplicantService.java
@@ -1,9 +1,13 @@
 package synk.meeteam.domain.recruitment.recruitment_applicant.service;
 
+import static synk.meeteam.domain.recruitment.recruitment_applicant.exception.RecruitmentApplicantExceptionType.INVALID_REQUEST;
+
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import synk.meeteam.domain.recruitment.recruitment_applicant.entity.RecruitmentApplicant;
+import synk.meeteam.domain.recruitment.recruitment_applicant.exception.RecruitmentApplicantException;
 import synk.meeteam.domain.recruitment.recruitment_applicant.repository.RecruitmentApplicantRepository;
 
 @Service
@@ -14,5 +18,33 @@ public class RecruitmentApplicantService {
     @Transactional
     public void registerRecruitmentApplicant(RecruitmentApplicant recruitmentApplicant) {
         recruitmentApplicantRepository.save(recruitmentApplicant);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RecruitmentApplicant> getAllApplicants(List<Long> applicantIds) {
+        return recruitmentApplicantRepository.findAllById(applicantIds);
+    }
+
+    @Transactional
+    public void approveApplicants(List<Long> applicantIds, List<RecruitmentApplicant> applicants, Long userId) {
+        validateCanApprove(applicants, userId);
+        validateApplicantCount(applicantIds.size(), applicants.size());
+
+        recruitmentApplicantRepository.bulkApprove(applicantIds);
+    }
+
+    private void validateCanApprove(List<RecruitmentApplicant> applicants, Long userId) {
+        // applicants 검증로직
+        // 호출한 사용자가 구인글 작성자인지 확인
+        // status가 다 NONE인지 확인
+
+        applicants.stream()
+                .forEach(applicant -> applicant.validateCanApprove(userId));
+    }
+
+    private void validateApplicantCount(int requestCount, int actualCount) {
+        if (requestCount != actualCount) {
+            throw new RecruitmentApplicantException(INVALID_REQUEST);
+        }
     }
 }
