bcba5a4fb6d2d5762c2f3217c5b33037d2599763
mikekks <thdalsrb79@naver.com>
2024-04-04 14:55:29 +0900

    [FEAT] 신청자 조회 페치조인 구현

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/repository/RecruitmentApplicantRepository.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/repository/RecruitmentApplicantRepository.java
@@ -1,7 +1,14 @@
 package synk.meeteam.domain.recruitment.recruitment_applicant.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import synk.meeteam.domain.recruitment.recruitment_applicant.entity.RecruitmentApplicant;
 
-public interface RecruitmentApplicantRepository extends JpaRepository<RecruitmentApplicant, Long> {
+public interface RecruitmentApplicantRepository extends JpaRepository<RecruitmentApplicant, Long>,
+        RecruitmentApplicantCustomRepository {
+
+    @Query("SELECT a FROM RecruitmentApplicant a JOIN FETCH a.recruitmentPost WHERE a.id IN :ids")
+    List<RecruitmentApplicant> findAllById(@Param("ids") List<Long> applicantIds);
 }
