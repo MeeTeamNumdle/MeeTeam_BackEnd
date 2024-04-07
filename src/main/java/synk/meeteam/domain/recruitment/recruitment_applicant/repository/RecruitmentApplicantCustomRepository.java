1f80f93493856e1730dda1820bc090cc8053206e
mikekks <thdalsrb79@naver.com>
2024-04-08 01:05:07 +0900

    [CHORE] 벌크 업데이트 제너릭하게 변경

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/repository/RecruitmentApplicantCustomRepository.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_applicant/repository/RecruitmentApplicantCustomRepository.java
@@ -1,7 +1,8 @@
 package synk.meeteam.domain.recruitment.recruitment_applicant.repository;
 
 import java.util.List;
+import synk.meeteam.domain.recruitment.recruitment_applicant.entity.RecruitStatus;
 
 public interface RecruitmentApplicantCustomRepository {
-    long bulkApprove(List<Long> applicantIds);
+    long bulkProcess(List<Long> applicantIds, RecruitStatus recruitStatus);
 }
