e6dbd76907522687d7f233434a6af159d5604995
goder-0 <goder0@naver.com>
2024-04-10 07:46:43 +0900

    [ADD] 포트폴리오 스킬 조회 repository 메소드 추가

--- src/main/java/synk/meeteam/domain/portfolio/portfolio_skill/repository/PortfolioSkillRepository.java
+++ src/main/java/synk/meeteam/domain/portfolio/portfolio_skill/repository/PortfolioSkillRepository.java
@@ -2,11 +2,14 @@ package synk.meeteam.domain.portfolio.portfolio_skill.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import synk.meeteam.domain.portfolio.portfolio.entity.Portfolio;
 import synk.meeteam.domain.portfolio.portfolio_skill.entity.PortfolioSkill;
 
 public interface PortfolioSkillRepository extends JpaRepository<PortfolioSkill, Long> {
-    List<PortfolioSkill> findAllByPortfolio(Portfolio portfolio);
+    @Query("SELECT ps FROM PortfolioSkill ps JOIN FETCH ps.skill WHERE ps.portfolio=:portfolio")
+    List<PortfolioSkill> findAllByPortfolioWithSkill(@Param("portfolio") Portfolio portfolio);
 
     void deleteAllByPortfolio(Portfolio portfolio);
 }
