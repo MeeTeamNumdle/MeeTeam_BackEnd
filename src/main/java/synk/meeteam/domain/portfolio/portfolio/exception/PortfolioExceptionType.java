c937b650ffffe1e253c82b3a05de67f16a17f9c4
goder-0 <goder0@naver.com>
2024-04-10 03:59:36 +0900

    [CHORE] 포트폴리오 예외 타입 추가

--- src/main/java/synk/meeteam/domain/portfolio/portfolio/exception/PortfolioExceptionType.java
+++ src/main/java/synk/meeteam/domain/portfolio/portfolio/exception/PortfolioExceptionType.java
@@ -8,7 +8,8 @@ import synk.meeteam.global.common.exception.ExceptionType;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum PortfolioExceptionType implements ExceptionType {
     NOT_FOUND_PORTFOLIO(HttpStatus.BAD_REQUEST, "포트폴리오를 찾을 수 없습니다."),
-    OVER_MAX_PIN_SIZE(HttpStatus.BAD_REQUEST, "포트폴리오의 최대 핀 개수를 초과합니다.");
+    OVER_MAX_PIN_SIZE(HttpStatus.BAD_REQUEST, "포트폴리오의 최대 핀 개수를 초과합니다."),
+    NOT_YOUR_PORTFOLIO(HttpStatus.BAD_REQUEST, "본인의 포트폴리오가 아닙니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
