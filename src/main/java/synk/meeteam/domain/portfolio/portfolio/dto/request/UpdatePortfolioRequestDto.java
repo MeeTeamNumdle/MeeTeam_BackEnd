e14aa06322c6bac7b26034ae1446c057b70201a2
goder-0 <goder0@naver.com>
2024-04-09 11:48:27 +0900

    [CHORE] 파일 순서 관련 필드 추가

--- src/main/java/synk/meeteam/domain/portfolio/portfolio/dto/request/UpdatePortfolioRequestDto.java
+++ src/main/java/synk/meeteam/domain/portfolio/portfolio/dto/request/UpdatePortfolioRequestDto.java
@@ -2,6 +2,7 @@ package synk.meeteam.domain.portfolio.portfolio.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
+import java.util.List;
 
 public record UpdatePortfolioRequestDto(
         @Schema(description = "제목", example = "Meeteam")
@@ -19,6 +20,8 @@ public record UpdatePortfolioRequestDto(
                 + "밋팀(Meeteam)은 나 자신을 의미하는 Me, 팀을 의미하는 Team, 만남을 의미하는 Meet이 합쳐진 단어입니다.\n"
                 + "대학생들의 보다 원활한 팀프로젝트를 위해 기획하게 되었으며, 그 외에 포토폴리오로서의 기능까지 생각하고 있습니다!\n")
         String content,
+        @Schema(description = "zip 파일 순서", example = "[\"이미지1.png\",\"이미지2.jpg\"]")
+        List<String> fileOrder,
         @Schema(description = "분야", example = "개발")
         String field,
         @Schema(description = "역할", example = "프론트엔드 개발자")
