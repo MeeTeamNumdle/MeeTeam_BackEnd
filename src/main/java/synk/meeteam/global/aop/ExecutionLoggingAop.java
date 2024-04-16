daddbbefae80343bc3732f89d3b1b1f40e48757f
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-04-16 18:52:46 +0900

    [INFRA] AOP 활용한 로깅 시스템 구축 (#237)
    * [FEAT] aop 기반 실행시간 로깅 구현
    
    * [INFRA] 볼륨간의 마운트 추가
    
    * [INFRA] RollingFileAppender 추가
    
    * [INFRA] RollingFileAppender 추가
    
    * [FEAT] aop 인터셉터 구현
    
    * [FEAT] userId 반환 추가

---
+++ src/main/java/synk/meeteam/global/aop/ExecutionLoggingAop.java
@@ -0,0 +1,70 @@
+package synk.meeteam.global.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import synk.meeteam.security.CustomAuthUser;
+
+@Aspect
+@Component
+@Log4j2
+public class ExecutionLoggingAop {
+
+    // 모든 패키지 내의 controller package에 존재하는 클래스
+    @Around("execution(* *..api.*.*(..))")
+    public void logExecutionTrace(ProceedingJoinPoint pjp) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        RequestMethod httpMethod = RequestMethod.valueOf(request.getMethod());
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long userId = 0L;
+        if (!authentication.getPrincipal().equals("anonymousUser")) {
+            CustomAuthUser customAuthUser = (CustomAuthUser) authentication.getPrincipal();
+            userId = customAuthUser.getUser().getId();
+        }
+
+        String className = pjp.getSignature().getDeclaringType().getSimpleName();
+        String methodName = pjp.getSignature().getName();
+        String task = className + "." + methodName;
+
+        log.info("[Call Method] " + httpMethod.toString() + ": " + task + " | Request userId=" + userId.toString());
+
+        Object[] paramArgs = pjp.getArgs();
+        for (Object object : paramArgs) {
+            if (Objects.nonNull(object)) {
+                log.info("[parameter type] {}", object.getClass().getSimpleName());
+                log.info("[parameter value] {}", object);
+            }
+        }
+
+        // 해당 클래스 처리 전의 시간
+        StopWatch sw = new StopWatch();
+        sw.start();
+
+        // 해당 클래스의 메소드 실행
+        try{
+            Object result = pjp.proceed();
+        }
+        catch (Exception e){
+            log.warn("[ERROR] " + task + " 메서드 예외 발생 : " + e.getMessage());
+            throw e;
+        }
+
+        // 해당 클래스 처리 후의 시간
+        sw.stop();
+        long executionTime = sw.getTotalTimeMillis();
+
+        log.info("[ExecutionTime] " + task + " --> " + executionTime + " (ms)");
+    }
+
+}
