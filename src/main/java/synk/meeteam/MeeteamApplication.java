c2d4652e096196a285b5132c6976ff9fa666e5de
goder-0 <goder0@naver.com>
2024-04-17 18:05:46 +0900

    [ADD] EnableScheduling 어노테이션 추가

--- src/main/java/synk/meeteam/MeeteamApplication.java
+++ src/main/java/synk/meeteam/MeeteamApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 @EnableAspectJAutoProxy
 public class MeeteamApplication {
 
