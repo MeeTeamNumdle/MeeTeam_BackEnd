e93fad06b6aa8d026b58f323b45110315ceab832
goder-0 <goder0@naver.com>
2024-04-17 18:34:23 +0900

    [CHORE] Spring TimeZone 설정 추가

--- src/main/java/synk/meeteam/MeeteamApplication.java
+++ src/main/java/synk/meeteam/MeeteamApplication.java
@@ -1,7 +1,9 @@
 package synk.meeteam;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +19,11 @@ public class MeeteamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MeeteamApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
     @Bean
