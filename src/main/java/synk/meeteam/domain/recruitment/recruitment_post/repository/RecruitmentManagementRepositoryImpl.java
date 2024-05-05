e5c250c2fb52a3dc0c257dfcbb99f273c4a28bc3
goder-0 <goder0@naver.com>
2024-05-05 14:36:54 +0900

    [FIX] Util 메소드 추가 및 구인글관리 Repository 재구현 - 함수를 하나로 압축 - 북마크 여부 오류 해결

--- src/main/java/synk/meeteam/domain/recruitment/recruitment_post/repository/RecruitmentManagementRepositoryImpl.java
+++ src/main/java/synk/meeteam/domain/recruitment/recruitment_post/repository/RecruitmentManagementRepositoryImpl.java
@@ -3,7 +3,10 @@ package synk.meeteam.domain.recruitment.recruitment_post.repository;
 import static synk.meeteam.domain.recruitment.bookmark.entity.QBookmark.bookmark;
 import static synk.meeteam.domain.recruitment.recruitment_applicant.entity.QRecruitmentApplicant.recruitmentApplicant;
 import static synk.meeteam.domain.recruitment.recruitment_post.entity.QRecruitmentPost.recruitmentPost;
-import static synk.meeteam.domain.recruitment.recruitment_post.repository.expression.ExpressionUtils.isClosedEq;
+import static synk.meeteam.domain.recruitment.recruitment_post.repository.RecruitmentManagementRepositoryImpl.managementJpaUtils.joinByManagementType;
+import static synk.meeteam.domain.recruitment.recruitment_post.repository.expression.RecruitmentExpressionUtils.isClosedEq;
+import static synk.meeteam.domain.recruitment.recruitment_post.repository.expression.RecruitmentExpressionUtils.isNotDeleted;
+import static synk.meeteam.domain.recruitment.recruitment_post.repository.expression.RecruitmentExpressionUtils.isWriter;
 
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -14,6 +17,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
+import synk.meeteam.domain.recruitment.recruitment_post.dto.ManageType;
+import synk.meeteam.domain.recruitment.recruitment_post.repository.expression.RecruitmentExpressionUtils;
 import synk.meeteam.domain.recruitment.recruitment_post.repository.vo.QRecruitmentPostVo;
 import synk.meeteam.domain.recruitment.recruitment_post.repository.vo.RecruitmentPostVo;
 import synk.meeteam.domain.user.user.entity.QUser;
@@ -25,28 +30,15 @@ import synk.meeteam.global.entity.DeleteStatus;
 public class RecruitmentManagementRepositoryImpl implements RecruitmentManagementRepository {
     private final JPAQueryFactory queryFactory;
 
-    @Override
-    public Page<RecruitmentPostVo> findMyBookmarkPost(Pageable pageable, User user, Boolean isClosed) {
-        List<RecruitmentPostVo> contents = getBookmarkPostVos(pageable, user, isClosed);
-        JPAQuery<Long> countQuery = getBookmarkCount(user, isClosed);
+    public Page<RecruitmentPostVo> findManagementPost(Pageable pageable, User user, Boolean isClosed,
+                                                      ManageType manageType) {
+        List<RecruitmentPostVo> contents = getManagementPostVos(pageable, user, isClosed, manageType);
+        JPAQuery<Long> countQuery = getManagementPostCount(user, isClosed, manageType);
         return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
     }
 
-    @Override
-    public Page<RecruitmentPostVo> findMyAppliedPost(Pageable pageable, User user, Boolean isClosed) {
-        List<RecruitmentPostVo> contents = getAppliedPostVos(pageable, user, isClosed);
-        JPAQuery<Long> countQuery = getAppliedCount(user, isClosed);
-        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
-    }
-
-    @Override
-    public Page<RecruitmentPostVo> findMyPost(Pageable pageable, User user, Boolean isClosed) {
-        List<RecruitmentPostVo> contents = getMyPostVos(pageable, user, isClosed);
-        JPAQuery<Long> countQuery = getMyPostCount(user, isClosed);
-        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
-    }
-
-    private List<RecruitmentPostVo> getBookmarkPostVos(Pageable pageable, User userDomain, Boolean isClosed) {
+    private List<RecruitmentPostVo> getManagementPostVos(Pageable pageable, User userDomain, Boolean isClosed,
+                                                         ManageType manageType) {
         QUser writer = new QUser("writer");
 
         JPAQuery<RecruitmentPostVo> query = queryFactory.select(
@@ -59,134 +51,58 @@ public class RecruitmentManagementRepositoryImpl implements RecruitmentManagemen
                                 writer.nickname,
                                 writer.profileImgFileName,
                                 recruitmentPost.deadline,
-                                Expressions.asBoolean(true),
+                                manageType == ManageType.BOOKMARKED ? Expressions.asBoolean(true)
+                                        : RecruitmentExpressionUtils.isBookmark(userDomain),
                                 recruitmentPost.createdAt,
                                 recruitmentPost.isClosed
                         )
                 )
                 .distinct()
+                .from(recruitmentPost)
+                .leftJoin(writer).on(recruitmentPost.createdBy.eq(writer.id))
+                .where(
+                        isClosedEq(isClosed),
+                        isNotDeleted(),
+                        isWriter(manageType, userDomain, writer)
+                );
+
+        joinByManagementType(userDomain, manageType, query);
+
+        return query.orderBy(recruitmentPost.createdAt.desc(), recruitmentPost.id.desc())
+                .offset(pageable.getOffset()) //페이지 번호
+                .limit(pageable.getPageSize()) //페이지 사이즈
+                .fetch();
+    }
+
+    private JPAQuery<Long> getManagementPostCount(User userDomain, Boolean isClosed, ManageType manageType) {
+        QUser writer = new QUser("writer");
+
+        JPAQuery<Long> query = queryFactory.select(recruitmentPost.countDistinct())
                 .from(recruitmentPost)
                 .leftJoin(writer).on(recruitmentPost.createdBy.eq(writer.id))
                 .leftJoin(bookmark).on(recruitmentPost.id.eq(bookmark.recruitmentPost.id))
                 .where(
                         isClosedEq(isClosed),
-                        bookmark.user.id.eq(userDomain.getId()),
-                        recruitmentPost.deleteStatus.ne(DeleteStatus.DELETED)
+                        isNotDeleted(),
+                        isWriter(manageType, userDomain, writer)
                 );
+        joinByManagementType(userDomain, manageType, query);
 
-        return query.orderBy(recruitmentPost.createdAt.desc(), recruitmentPost.id.desc())
-                .offset(pageable.getOffset()) //페이지 번호
-                .limit(pageable.getPageSize()) //페이지 사이즈
-                .fetch();
+        return query;
     }
 
-    private List<RecruitmentPostVo> getAppliedPostVos(Pageable pageable, User userDomain, Boolean isClosed) {
-        QUser writer = new QUser("writer");
-
-        JPAQuery<RecruitmentPostVo> query = queryFactory.select(
-                        new QRecruitmentPostVo(
-                                recruitmentPost.id,
-                                recruitmentPost.title,
-                                recruitmentPost.category,
-                                recruitmentPost.scope,
-                                writer.id,
-                                writer.nickname,
-                                writer.profileImgFileName,
-                                recruitmentPost.deadline,
-                                Expressions.asBoolean(true),
-                                recruitmentPost.createdAt,
-                                recruitmentPost.isClosed
-                        )
-                )
-                .distinct()
-                .from(recruitmentPost)
-                .leftJoin(writer).on(recruitmentPost.createdBy.eq(writer.id))
-                .leftJoin(recruitmentApplicant).on(recruitmentPost.id.eq(recruitmentApplicant.recruitmentPost.id))
-                .where(
-                        isClosedEq(isClosed),
-                        recruitmentApplicant.applicant.id.eq(userDomain.getId()),
-                        recruitmentApplicant.deleteStatus.ne(DeleteStatus.DELETED),
-                        recruitmentPost.deleteStatus.ne(DeleteStatus.DELETED)
-                );
-
-        return query.orderBy(recruitmentPost.createdAt.desc(), recruitmentPost.id.desc())
-                .offset(pageable.getOffset()) //페이지 번호
-                .limit(pageable.getPageSize()) //페이지 사이즈
-                .fetch();
-    }
-
-    private List<RecruitmentPostVo> getMyPostVos(Pageable pageable, User userDomain, Boolean isClosed) {
-        QUser writer = new QUser("writer");
-
-        JPAQuery<RecruitmentPostVo> query = queryFactory.select(
-                        new QRecruitmentPostVo(
-                                recruitmentPost.id,
-                                recruitmentPost.title,
-                                recruitmentPost.category,
-                                recruitmentPost.scope,
-                                writer.id,
-                                writer.nickname,
-                                writer.profileImgFileName,
-                                recruitmentPost.deadline,
-                                Expressions.asBoolean(true),
-                                recruitmentPost.createdAt,
-                                recruitmentPost.isClosed
-                        )
-                )
-                .distinct()
-                .from(recruitmentPost)
-                .leftJoin(writer).on(recruitmentPost.createdBy.eq(writer.id))
-                .where(
-                        isClosedEq(isClosed),
-                        writer.id.eq(userDomain.getId()),
-                        recruitmentPost.deleteStatus.ne(DeleteStatus.DELETED)
-                );
-
-        return query.orderBy(recruitmentPost.createdAt.desc(), recruitmentPost.id.desc())
-                .offset(pageable.getOffset()) //페이지 번호
-                .limit(pageable.getPageSize()) //페이지 사이즈
-                .fetch();
-    }
-
-    private JPAQuery<Long> getBookmarkCount(User userDomain, Boolean isClosed) {
-        QUser writer = new QUser("writer");
-
-        return queryFactory.select(recruitmentPost.countDistinct())
-                .from(recruitmentPost)
-                .leftJoin(writer).on(recruitmentPost.createdBy.eq(writer.id))
-                .leftJoin(bookmark).on(recruitmentPost.id.eq(bookmark.recruitmentPost.id))
-                .where(
-                        isClosedEq(isClosed),
-                        bookmark.user.id.eq(userDomain.getId()),
-                        recruitmentPost.deleteStatus.ne(DeleteStatus.DELETED)
-                );
-    }
-
-    private JPAQuery<Long> getAppliedCount(User userDomain, Boolean isClosed) {
-        QUser writer = new QUser("writer");
-
-        return queryFactory.select(recruitmentPost.countDistinct())
-                .from(recruitmentPost)
-                .leftJoin(writer).on(recruitmentPost.createdBy.eq(writer.id))
-                .leftJoin(recruitmentApplicant).on(recruitmentPost.id.eq(recruitmentApplicant.recruitmentPost.id))
-                .where(
-                        isClosedEq(isClosed),
-                        recruitmentApplicant.applicant.id.eq(userDomain.getId()),
-                        recruitmentPost.deleteStatus.ne(DeleteStatus.DELETED)
-                );
-    }
-
-    private JPAQuery<Long> getMyPostCount(User userDomain, Boolean isClosed) {
-        QUser writer = new QUser("writer");
-
-        return queryFactory.select(recruitmentPost.countDistinct())
-                .from(recruitmentPost)
-                .leftJoin(writer).on(recruitmentPost.createdBy.eq(writer.id))
-                .where(
-                        isClosedEq(isClosed),
-                        writer.id.eq(userDomain.getId()),
-                        recruitmentPost.deleteStatus.ne(DeleteStatus.DELETED)
-                );
+    static class managementJpaUtils {
+        static <T> void joinByManagementType(User userDomain, ManageType manageType, JPAQuery<T> query) {
+            if (manageType.equals(ManageType.BOOKMARKED)) {
+                query.leftJoin(bookmark).on(recruitmentPost.id.eq(bookmark.recruitmentPost.id))
+                        .where(bookmark.user.id.eq(userDomain.getId()));
+            } else if (manageType.equals(ManageType.APPLIED)) {
+                query.leftJoin(recruitmentApplicant)
+                        .on(recruitmentPost.id.eq(recruitmentApplicant.recruitmentPost.id))
+                        .where(recruitmentApplicant.applicant.id.eq(userDomain.getId()),
+                                recruitmentApplicant.deleteStatus.ne(DeleteStatus.DELETED));
+            }
+        }
     }
 
 }
