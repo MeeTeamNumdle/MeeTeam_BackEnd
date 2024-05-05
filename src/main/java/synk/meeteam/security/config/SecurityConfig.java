3ad79f71b3eec5bc809fc4582d1972fab8f194e7
goder-0 <goder0@naver.com>
2024-05-06 03:34:37 +0900

    [CHORE] 로그인 안한 유저도 포트폴리오 조회 가능하도록 whitelist 추가

--- src/main/java/synk/meeteam/security/config/SecurityConfig.java
+++ src/main/java/synk/meeteam/security/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
             "/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**", "/actuator/health",
 
             "/skill/**", "/tag/**", "/role/**",
-            "/user/encrypt/**"
+            "/user/encrypt/**", "/portfolio/{id}"
     };
 
     private static final String[] SEMI_AUTH_WHITELIST = {
@@ -61,9 +61,10 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://meeteam.co.kr", "https://www.meeteam.co.kr","http://localhost:5173",
+                        .allowedOrigins("https://meeteam.co.kr", "https://www.meeteam.co.kr", "http://localhost:5173",
                                 "http://localhost:8080", "https://api.meeteam.co.kr")
-                        .allowedOriginPatterns("https://meeteam.co.kr", "https://www.meeteam.co.kr", "http://localhost:5173",
+                        .allowedOriginPatterns("https://meeteam.co.kr", "https://www.meeteam.co.kr",
+                                "http://localhost:5173",
                                 "http://localhost:8080", "https://api.meeteam.co.kr")
                         .allowedMethods(
                                 HttpMethod.GET.name(),
