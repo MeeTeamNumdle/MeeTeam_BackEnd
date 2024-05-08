1db1a40af12d81df57c643ee703284faaea6cbdd
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-05-09 00:41:34 +0900

    [CHORE] 로그아웃 및 토큰 재발행 로직 수정 (#339)
    * [CHORE] 플랫폼 id -> userId로 변경
    
    * [CHORE] 플랫폼 id -> userId로 변경

--- src/main/java/synk/meeteam/infra/redis/repository/RedisTokenRepository.java
+++ src/main/java/synk/meeteam/infra/redis/repository/RedisTokenRepository.java
@@ -2,7 +2,6 @@ package synk.meeteam.infra.redis.repository;
 
 import static synk.meeteam.domain.auth.exception.AuthExceptionType.NOT_FOUND_REFRESH_TOKEN;
 
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import synk.meeteam.domain.auth.exception.AuthException;
@@ -11,12 +10,9 @@ import synk.meeteam.security.jwt.service.vo.TokenVO;
 
 @Repository
 public interface RedisTokenRepository extends CrudRepository<TokenVO, String> {
-    Optional<TokenVO> findByPlatformId(String platformId);
-
-    default TokenVO findByPlatformIdOrElseThrowException(String platformId) {
-        return findByPlatformId(platformId)
+    default TokenVO findByIdOrElseThrow(String userId) {
+        return findById(userId)
                 .filter(tokenVO -> !tokenVO.isBlack())
-                .orElseThrow(
-                        () -> new AuthException(NOT_FOUND_REFRESH_TOKEN));
+                .orElseThrow(() -> new AuthException(NOT_FOUND_REFRESH_TOKEN));
     }
 }
